50531f315577f71030419b5a71044e2646897a84
Ravi Kotecha <kotecha.ravi@gmail.com>
2015-06-19 15:24:50 +0100

    Make Authenticated API developer friendly.
    This documents how to use the API for a new developer,
    adds a Postman collection and updates the tests so they
    take into account that each user can only see their own prison's
    transactions.

--- mtp_api/apps/transaction/tests/test_views.py
+++ mtp_api/apps/transaction/tests/test_views.py
@@ -1,11 +1,19 @@
+from rest_framework.settings import api_settings
+from collections import Counter
+
 from django.core.urlresolvers import reverse
 
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from core.tests.utils import make_test_users, make_test_oauth_applications
+from transaction.tests.utils import generate_transactions
+from mtp_auth.models import PrisonUserMapping
 from transaction.models import Transaction
 
-from transaction.tests.utils import generate_transactions
+
+def get_user_for_prison(prison):
+    return PrisonUserMapping.objects.filter(prisons=prison).first().user
 
 
 class TransactionViewTestCase(APITestCase):
@@ -13,7 +21,10 @@ class TransactionViewTestCase(APITestCase):
 
     def setUp(self):
         super(TransactionViewTestCase, self).setUp()
-        generate_transactions(uploads=2, transaction_batch=21)
+        self.transactions = generate_transactions(
+            uploads=2, transaction_batch=101)
+        make_test_users()
+        make_test_oauth_applications()
 
     def assertTransactionsInUpload(self, response, upload_counter):
         """
@@ -28,26 +39,57 @@ class TransactionViewTestCase(APITestCase):
         self.assertEqual(len(uc), 1)
         self.assertEqual(uc[0], upload_counter)
 
+    def calculate_prison_transaction_counts(self, upload_counter=None):
+        if not upload_counter:
+            upload_counter = max({x.upload_counter for x in self.transactions})
+
+        return Counter([x.prison for x in self.transactions if x.upload_counter == upload_counter])
+
     def test_list_latest_upload(self):
         url = reverse('transaction-list')
+        unique_prisons = self.calculate_prison_transaction_counts()
+        for prison, count in unique_prisons.items():
+            # we have to do if Prison because some transactions don't have a prison attached
+            # and therefore no user will be able to see those...
+            # TODO: figure out who is responsible for these transactions
+            if prison:
+                self.client.force_authenticate(
+                    user=get_user_for_prison(prison))
+                response = self.client.get(url, format='json')
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
+                self.assertEqual(response.data['count'], count)
+                if count > api_settings.PAGE_SIZE:
+                    self.assertNotEqual(response.data['next'], None)
 
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['count'], 21)
-        self.assertNotEqual(response.data['next'], None)
-
-        # get transaction from response data and make sure that they
-        # all belong to last upload
-        self.assertTransactionsInUpload(response, 2)
+                # get transaction from response data and make sure that they
+                # all belong to last upload
+                self.assertTransactionsInUpload(response, 2)
 
     def test_list_specific_upload(self):
-        url = '%s?upload_counter=1' % reverse('transaction-list')
+        upload_counter = 1
+        url = '{url}?upload_counter={upload_counter}'.\
+            format(
+                url=reverse('transaction-list'),
+                upload_counter=upload_counter
+            )
 
+        unique_prisons = self.calculate_prison_transaction_counts(
+            upload_counter=upload_counter)
+        for prison, count in unique_prisons.items():
+            if prison:
+                self.client.force_authenticate(
+                    user=get_user_for_prison(prison))
+                response = self.client.get(url, format='json')
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
+                self.assertEqual(response.data['count'], count)
+                if count > api_settings.PAGE_SIZE:
+                    self.assertNotEqual(response.data['next'], None)
+
+                # get transaction from response data and make sure that they
+                # all belong to last upload
+                self.assertTransactionsInUpload(response, 1)
+
+    def test_list_cant_access_if_not_authenticated(self):
+        url = reverse('transaction-list')
         response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['count'], 21)
-        self.assertNotEqual(response.data['next'], None)
-
-        # get transaction from response data and make sure that they
-        # all belong to last upload
-        self.assertTransactionsInUpload(response, 1)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
