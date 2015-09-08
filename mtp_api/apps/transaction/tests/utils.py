fc144284977a7715a9f73c711cf35eb06df8b351
Marco Fucci <info@marcofucci.com>
2015-09-08 16:14:00 +0100

    Create PrisonerLocations in generate_transactions util func
    This creates prisoner location records when generating the transactions
    and changes generate_transactions to use that data instead of random choosen prisons.
    The PrisonerLocation records are not used in any matching query yet but
    will be in the near future and this will make the switch easier.

--- mtp_api/apps/transaction/tests/utils.py
+++ mtp_api/apps/transaction/tests/utils.py
@@ -1,5 +1,6 @@
 import datetime
 import random
+from itertools import cycle
 
 from django.utils import timezone
 from django.utils.crypto import get_random_string
@@ -10,40 +11,8 @@ from prison.models import Prison
 from transaction.models import Transaction
 from transaction.constants import TRANSACTION_STATUS
 
-
-from prison.tests.utils import random_prisoner_number, random_prisoner_dob
-
-
-class PrisonChooser(object):
-
-    def __init__(self):
-        self.prisons = Prison.objects.all()
-        self.clerks_per_prison = {}
-        for prison in self.prisons:
-            user_ids = prison.prisonusermapping_set.values_list('user', flat=True)
-            self.clerks_per_prison[prison.pk] = {
-                'users': User.objects.filter(id__in=user_ids),
-                'index': 0
-            }
-        self.index = 0
-
-    def _choose(self, l, index):
-        item = l[index]
-        index += 1
-        if index >= len(l):
-            index = 0
-        return (item, index)
-
-    def choose_prison(self):
-        prison, index = self._choose(self.prisons, self.index)
-        self.index = index
-        return prison
-
-    def choose_user(self, prison):
-        data = self.clerks_per_prison[prison.pk]
-        user, index = self._choose(data['users'], data['index'])
-        data['index'] = index
-        return user
+from prison.tests.utils import random_prisoner_number, random_prisoner_dob, \
+    get_prisoner_location_creator
 
 
 def random_reference(prisoner_number=None, prisoner_dob=None):
@@ -58,7 +27,6 @@ def random_reference(prisoner_number=None, prisoner_dob=None):
 def generate_initial_transactions_data(tot=50):
     data_list = []
 
-    prison_chooser = PrisonChooser()
     now = timezone.now().replace(microsecond=0)
 
     for transaction_counter in range(1, tot+1):
@@ -75,7 +43,6 @@ def generate_initial_transactions_data(tot=50):
 
         data = {
             'amount': random.randint(1000, 30000),
-            'prison': None,
             'received_at': now - datetime.timedelta(
                 minutes=random.randint(0, 10000)
             ),
@@ -88,9 +55,7 @@ def generate_initial_transactions_data(tot=50):
         }
 
         if include_prisoner_info:
-            prison = prison_chooser.choose_prison()
             data.update({
-                'prison': prison,
                 'prisoner_number': random_prisoner_number(),
                 'prisoner_dob': random_prisoner_dob()
             })
@@ -107,39 +72,56 @@ def generate_initial_transactions_data(tot=50):
     return data_list
 
 
-def generate_transactions_data(transaction_batch=50):
+def generate_transactions(transaction_batch=50):
+    def get_owner_and_status_chooser():
+        clerks_per_prison = {}
+        for prison in Prison.objects.all():
+            user_ids = prison.prisonusermapping_set.values_list('user', flat=True)
+            clerks_per_prison[prison.pk] = (
+                cycle(list(User.objects.filter(id__in=user_ids))),
+                cycle([
+                    TRANSACTION_STATUS.LOCKED,
+                    TRANSACTION_STATUS.AVAILABLE,
+                    TRANSACTION_STATUS.CREDITED
+                ])
+            )
+
+        def internal_function(prison):
+            user, status = clerks_per_prison[prison.pk]
+            return (next(user), next(status))
+        return internal_function
+
     data_list = generate_initial_transactions_data(
         tot=transaction_batch
     )
 
-    prison_chooser = PrisonChooser()
+    location_creator = get_prisoner_location_creator()
+    onwer_status_chooser = get_owner_and_status_chooser()
 
+    transactions = []
     for transaction_counter, data in enumerate(data_list, start=1):
-        prison = data['prison']
-
-        if prison:
+        is_valid, prisoner_location = location_creator(
+            data.get('prisoner_number'), data.get('prisoner_dob')
+        )
+        if is_valid:
             # randomly choose the state of the transaction
-            trans_status = random.choice(
-                [
-                    TRANSACTION_STATUS.LOCKED,
-                    TRANSACTION_STATUS.AVAILABLE,
-                    TRANSACTION_STATUS.CREDITED
-                ]
-            )
+            prison = prisoner_location.prison
+            owner, t_status = onwer_status_chooser(prison)
 
-            if trans_status == TRANSACTION_STATUS.LOCKED:
+            data['prison'] = prison
+            if t_status == TRANSACTION_STATUS.LOCKED:
                 data.update({
-                    'owner': prison_chooser.choose_user(prison),
+                    'owner': owner,
                     'credited': False
                 })
-            elif trans_status == TRANSACTION_STATUS.AVAILABLE:
+            elif t_status == TRANSACTION_STATUS.AVAILABLE:
                 data.update({
                     'owner': None,
                     'credited': False
                 })
-            elif trans_status == TRANSACTION_STATUS.CREDITED:
+            elif t_status == TRANSACTION_STATUS.CREDITED:
                 data.update({
-                    'owner': prison_chooser.choose_user(prison),
+                    'owner': owner,
                     'credited': True
                 })
         else:
@@ -148,17 +130,8 @@ def generate_transactions_data(transaction_batch=50):
             else:
                 data.update({'refunded': False})
 
-    return data_list
-
-
-def generate_transactions(transaction_batch=30):
-    data_list = generate_transactions_data(
-        transaction_batch=transaction_batch
-    )
-
-    transactions = []
-    for data in data_list:
         transactions.append(
             Transaction.objects.create(**data)
         )
+
     return transactions
