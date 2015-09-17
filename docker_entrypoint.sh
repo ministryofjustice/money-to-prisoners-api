10ad0d275ea8d692341976a9f2fcd57397582451
Ian Brechin <ian.brechin@gmail.com>
2015-09-17 17:31:08 +0100

    Add a docker task to wipe and recreate the database for the env
    Runs the django-admin tasks flush, migrate, load_test_data

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 
-if [[ "${DOCKER_STATE}" == 'migrate' ]]
+if [[ "${DOCKER_STATE}" == 'reset_db' ]]
+then
+	echo "flushing db"
+	python manage.py flush --noinput --settings=mtp_api.settings.prod
+    echo "running migrate"
+    python manage.py migrate --settings=mtp_api.settings.prod
+    echo "loading test data"
+    python manage.py load_test_data --settings=mtp_api.settings.prod
+elif [[ "${DOCKER_STATE}" == 'migrate' ]]
 then
     echo "running migrate"
     python ./manage.py migrate --settings=mtp_api.settings.prod
