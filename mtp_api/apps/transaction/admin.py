01c451c7f7bddde4f29e9f670d9faf28cd8a6ace
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-11-09 12:39:46 +0000

    Add transaction log listings to Django admin so that the dates of actions can be obtained

--- mtp_api/apps/transaction/admin.py
+++ mtp_api/apps/transaction/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
 
-from .models import Transaction
+from .models import Transaction, Log
 
 
-class TransactionAdmin(ModelAdmin):
+class LogAdminInline(admin.TabularInline):
+    model = Log
+    extra = 0
+    fields = ('action', 'created', 'user')
+    readonly_fields = ('action', 'created', 'user')
+    ordering = ('-created',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class TransactionAdmin(admin.ModelAdmin):
     list_display = ('prisoner_name', 'prisoner_number', 'formatted_amount', 'sender_name',
                     'received_at', 'credited_at', 'refunded_at')
     ordering = ('-received_at',)
+    readonly_fields = ('credited', 'refunded')
+    inlines = (LogAdminInline,)
 
     @classmethod
     def formatted_amount(cls, instance):
