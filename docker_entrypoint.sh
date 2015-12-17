a4873a4a2de568e28b5a52f0adc090e46192ead9
Ian Brechin <ian.brechin@gmail.com>
2015-12-17 11:21:43 +0000

    Remove 'reset_db' docker task
    This functionality has been replaced by admin management
    commands.

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ "${DOCKER_STATE}" == 'reset_db' ]]
-then
-    echo "running migrate"
-    python manage.py migrate --settings=mtp_api.settings.prod --noinput
-    echo "flushing db"
-    python manage.py flush --noinput --settings=mtp_api.settings.prod --noinput
-    echo "running migrate (replacing fixtures)"
-    python manage.py migrate --settings=mtp_api.settings.prod --noinput
-    echo "loading test data"
-    python manage.py load_test_data --settings=mtp_api.settings.prod
-elif [[ "${DOCKER_STATE}" == 'migrate' ]]
+if [[ "${DOCKER_STATE}" == 'migrate' ]]
 then
     echo "running migrate"
     python ./manage.py migrate --settings=mtp_api.settings.prod --noinput
