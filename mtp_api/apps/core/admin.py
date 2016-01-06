c00d6265f1e0eb9f7dfe763869508b32c2c5d20a
Ian Brechin <ian.brechin@gmail.com>
2016-01-06 15:18:01 +0000

    Refactor out base admin form filter from date range filter
    This provides a base class that can be used to produce a form
    filter for admin pages that can have arbitrary fields.

--- mtp_api/apps/core/admin.py
+++ mtp_api/apps/core/admin.py
@@ -6,56 +6,80 @@ from django import forms
 from .forms import AdminFilterForm, SidebarDateWidget
 
 
-class DateRangeFilter(admin.FieldListFilter):
+class FormFilter(admin.FieldListFilter):
     template = 'core/admin_form_filter.html'
 
     def __init__(self, field, request, params, model, model_admin, field_path):
         self.field_generic = '%s__' % field_path
-
-        self.form_fields = [
-            (_('Start date'), '%s__gte' % field_path),
-            (_('End date'), '%s__lt' % field_path)
-        ]
+        self.field_path = field_path
         super().__init__(field, request, params, model, model_admin, field_path)
 
     def expected_parameters(self):
-        return ['%sgte' % self.field_generic, '%slt' % self.field_generic]
+        return list(next(zip(*self.get_form_fields())))
 
     def choices(self, cl):
         query_active = False
+        fields_to_render = self.get_form_fields()
 
-        fields_to_render = []
-        for k in cl.params:
-            if not k.startswith(self.field_generic):
-                fields_to_render.append(
-                    (k, forms.CharField(initial=cl.params[k],
-                                        widget=forms.HiddenInput()))
-                )
-
-        for label, param in self.form_fields:
-            initial = ''
+        for param, field in fields_to_render:
             if param in cl.params:
                 query_active = True
-                initial = cl.params[param]
-            fields_to_render.append(
-                (param, forms.DateField(widget=SidebarDateWidget(
-                    attrs={'placeholder': label}), initial=initial))
-            )
+                break
 
-        form = AdminFilterForm(extra_fields=fields_to_render)
+        for k in cl.params:
+            if not (k == self.field_path or k.startswith(self.field_generic)):
+                fields_to_render.append(
+                    (k, forms.CharField(widget=forms.HiddenInput()))
+                )
+
+        initial = {}
+        for name, field in fields_to_render:
+            initial[name] = cl.params.get(name, '')
+
+        form = AdminFilterForm(extra_fields=fields_to_render, initial=initial)
 
         return [
             {
                 'selected': not query_active,
-                'query_string': cl.get_query_string({}, [self.field_generic]),
-                'display': _('Any date')
+                'query_string': cl.get_query_string(
+                    {}, [self.field_path, self.field_generic]
+                ),
+                'display': _('All')
             },
             {
                 'selected': query_active,
                 'form': form,
-                'display': _('Date in range')
+                'display': self.get_submit_label()
             },
         ]
+
+    def get_form_fields(self):
+        """
+        Returns (db_field, form_field) list to be used in the filter.
+        """
+        raise NotImplementedError('subclasses of FormFilter must provide a get_form_fields() method')
+
+    def get_submit_label(self):
+        """
+        Returns the label for the form submit button.
+        """
+        raise NotImplementedError('subclasses of FormFilter must provide a get_submit_label() method')
+
+
+class DateRangeFilter(FormFilter):
+
+    def get_form_fields(self):
+        return [
+            ('%s__gte' % self.field_path, forms.DateField(
+                widget=SidebarDateWidget(attrs={'placeholder': _('Start date')})
+            )),
+            ('%s__lt' % self.field_path, forms.DateField(
+                widget=SidebarDateWidget(attrs={'placeholder': _('End date')})
+            ))
+        ]
+
+    def get_submit_label(self):
+        return _('Date in range')
 
 
 class RelatedAnyFieldListFilter(admin.RelatedFieldListFilter):
