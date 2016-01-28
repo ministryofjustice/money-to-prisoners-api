28c75c05d751c777a2810fd25dcd50af7bc80b0a
Ian Brechin <ian.brechin@gmail.com>
2016-01-28 16:39:45 +0000

    Refactor categorisation of transactions
    This returns category to being a binary choice (credit/debit) and
    instead stores information about where the transaction came from in
    the source field.

--- mtp_api/apps/payment/models.py
+++ mtp_api/apps/payment/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 from model_utils.models import TimeStampedModel
 
 from transaction.models import Transaction
-from transaction.constants import TRANSACTION_CATEGORY
+from transaction.constants import TRANSACTION_CATEGORY, TRANSACTION_SOURCE
 from transaction.signals import transaction_created, transaction_prisons_need_updating
 from .constants import PAYMENT_STATUS
 
@@ -37,7 +37,8 @@ def create_transaction_for_payment(sender, instance, **kwargs):
         transaction.prisoner_number = instance.prisoner_number
         transaction.prisoner_dob = instance.prisoner_dob
         transaction.amount = instance.amount
-        transaction.category = TRANSACTION_CATEGORY.ONLINE_CREDIT
+        transaction.category = TRANSACTION_CATEGORY.CREDIT
+        transaction.source = TRANSACTION_SOURCE.ONLINE
         transaction.received_at = instance.modified
         transaction.save()
 
