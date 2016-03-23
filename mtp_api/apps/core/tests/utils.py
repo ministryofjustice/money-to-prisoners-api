12f88b2608bc192f79dccecc8daa746334ce1d4c
Ian Brechin <ian.brechin@gmail.com>
2016-03-23 14:29:54 +0000

    Add UserAdmin group for users who can CRUD other users

--- mtp_api/apps/core/tests/utils.py
+++ mtp_api/apps/core/tests/utils.py
@@ -1,15 +1,20 @@
 from unittest import mock
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from oauth2_provider.models import Application
 
-from mtp_auth.constants import CASHBOOK_OAUTH_CLIENT_ID, \
-    BANK_ADMIN_OAUTH_CLIENT_ID, PRISONER_LOCATION_OAUTH_CLIENT_ID, \
-    SEND_MONEY_CLIENT_ID
-from mtp_auth.models import ApplicationUserMapping, PrisonUserMapping
-from mtp_auth.tests.mommy_recipes import create_prison_user_mapping, \
-    create_prisoner_location_admins, create_bank_admins, \
-    create_refund_bank_admins, create_send_money_shared_users
+from mtp_auth.constants import (
+    CASHBOOK_OAUTH_CLIENT_ID, BANK_ADMIN_OAUTH_CLIENT_ID,
+    PRISONER_LOCATION_OAUTH_CLIENT_ID, SEND_MONEY_CLIENT_ID
+)
+from mtp_auth.models import (
+    ApplicationUserMapping, PrisonUserMapping
+)
+from mtp_auth.tests.mommy_recipes import (
+    create_prison_clerk, create_prisoner_location_admin, create_bank_admin,
+    create_refund_bank_admin, create_send_money_shared_user, create_user_admin
+)
 from prison.models import Prison
 
 
@@ -42,14 +47,7 @@ class MockModelTimestamps:
 
 
 def make_applications():
-    user = get_user_model().objects.first()
-
-    for client_id in [
-        CASHBOOK_OAUTH_CLIENT_ID,
-        BANK_ADMIN_OAUTH_CLIENT_ID,
-        PRISONER_LOCATION_OAUTH_CLIENT_ID,
-        SEND_MONEY_CLIENT_ID,
-    ]:
+    def make_application(user, client_id, groups):
         Application.objects.get_or_create(
             client_id=client_id,
             client_type='confidential',
@@ -58,6 +56,18 @@ def make_applications():
             name=client_id,
             user=user
         )
+
+    user = get_user_model().objects.first()
+
+    make_application(user, CASHBOOK_OAUTH_CLIENT_ID,
+                     [Group.objects.get(name='PrisonClerk')])
+    make_application(user, PRISONER_LOCATION_OAUTH_CLIENT_ID,
+                     [Group.objects.get(name='PrisonerLocationAdmin')])
+    make_application(user, BANK_ADMIN_OAUTH_CLIENT_ID,
+                     [Group.objects.get(name='BankAdmin'),
+                      Group.objects.get(name='RefundBankAdmin')])
+    make_application(user, SEND_MONEY_CLIENT_ID,
+                     [Group.objects.get(name='SendMoney')])
 
 
 def give_superusers_full_access():
@@ -78,18 +88,17 @@ def make_test_users(clerks_per_prison=2):
     prison_clerks = []
     for prison in Prison.objects.all():
         for _ in range(clerks_per_prison):
-            pu = create_prison_user_mapping(prison)
-            prison_clerks.append(pu.user)
+            prison_clerks.append(create_prison_clerk(prison=prison))
 
     # prisoner location admin
-    prisoner_location_admins = create_prisoner_location_admins()
+    prisoner_location_admins = [create_prisoner_location_admin()]
 
     # bank admin
-    bank_admins = create_bank_admins()
-    refund_bank_admins = create_refund_bank_admins()
+    bank_admins = [create_bank_admin()]
+    refund_bank_admins = [create_refund_bank_admin()]
 
     # send money shared user
-    send_money_users = create_send_money_shared_users()
+    send_money_users = [create_send_money_shared_user()]
 
     # create test oauth applications
     make_applications()
@@ -110,3 +119,38 @@ def make_test_users(clerks_per_prison=2):
     return (prison_clerks, prisoner_location_admins,
             bank_admins, refund_bank_admins,
             send_money_users)
+
+
+def make_test_user_admins():
+    # prison user admins
+    prison_clerks = []
+    for prison in Prison.objects.all():
+        prison_clerks.append(create_user_admin(
+            create_prison_clerk, prison=prison, name_and_password='user-admin')
+        )
+
+    # prisoner location user admins
+    prisoner_location_admins = [
+        create_user_admin(create_prisoner_location_admin,
+                          name_and_password='pla-user-admin')]
+
+    # bank admin user admins
+    refund_bank_admins = [
+        create_user_admin(create_refund_bank_admin,
+                          name_and_password='rba-user-admin')]
+
+    # create test oauth applications
+    make_applications()
+
+    def link_users_with_client(users, client_id):
+        for user in users:
+            ApplicationUserMapping.objects.get_or_create(
+                user=user,
+                application=Application.objects.get(client_id=client_id)
+            )
+
+    link_users_with_client(prison_clerks, CASHBOOK_OAUTH_CLIENT_ID)
+    link_users_with_client(prisoner_location_admins, PRISONER_LOCATION_OAUTH_CLIENT_ID)
+    link_users_with_client(refund_bank_admins, BANK_ADMIN_OAUTH_CLIENT_ID)
+
+    return (prison_clerks, prisoner_location_admins, refund_bank_admins)
