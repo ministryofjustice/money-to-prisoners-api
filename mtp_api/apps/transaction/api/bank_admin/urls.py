4570b48091639fce2301742e843e8e354c39bf81
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-03-29 15:57:18 +0100

    Update namespace url conf structure

--- mtp_api/apps/transaction/api/bank_admin/urls.py
+++ mtp_api/apps/transaction/api/bank_admin/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'bank_admin'
 urlpatterns = [
     url(r'^transactions/$', views.TransactionView.as_view({
         'get': 'list',
