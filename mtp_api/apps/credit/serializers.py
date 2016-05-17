e3e5ed1983ef31c790904c681a6926033e15c748
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-05-17 12:40:25 +0100

    Add prison to sender-grouped security view and filter by sender name

--- mtp_api/apps/credit/serializers.py
+++ mtp_api/apps/credit/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from prison.models import Prison
 from .models import Credit
 
 
@@ -89,68 +90,38 @@ class LockedCreditSerializer(CreditSerializer):
 class BaseRecipientSerializer(serializers.Serializer):
     prisoner_number = serializers.CharField()
     prisoner_name = serializers.CharField()
+    prison_name = serializers.SerializerMethodField()
 
-    class Meta:
-        fields = (
-            'prisoner_number',
-            'prisoner_name'
-        )
+    @classmethod
+    def get_prison_name(cls, obj):
+        try:
+            return Prison.objects.get(pk=obj['prison_id']).name
+        except Prison.DoesNotExist:
+            return None
 
 
 class DetailRecipientSerializer(BaseRecipientSerializer):
     credit_total = serializers.IntegerField()
     credit_count = serializers.IntegerField()
 
-    class Meta:
-        fields = BaseRecipientSerializer.Meta.fields + (
-            'credit_total',
-            'credit_count',
-        )
-
 
 class BaseSenderSerializer(serializers.Serializer):
-    sender = serializers.CharField(required=False)
+    sender_name = serializers.CharField(required=False)
     sender_sort_code = serializers.CharField(required=False)
     sender_account_number = serializers.CharField(required=False)
     sender_roll_number = serializers.CharField(required=False)
-
-    class Meta:
-        fields = (
-            'sender',
-            'sender_sort_code',
-            'sender_account_number',
-            'sender_roll_number',
-        )
 
 
 class DetailSenderSerializer(BaseSenderSerializer):
     credit_total = serializers.IntegerField()
     credit_count = serializers.IntegerField()
 
-    class Meta:
-        fields = BaseSenderSerializer.Meta.fields + (
-            'credit_total',
-            'credit_count',
-        )
-
 
 class RecipientSerializer(BaseRecipientSerializer):
     sender_count = serializers.IntegerField()
     senders = DetailSenderSerializer(many=True)
 
-    class Meta:
-        fields = BaseRecipientSerializer.Meta.fields + (
-            'sender_count',
-            'senders',
-        )
-
 
 class SenderSerializer(BaseSenderSerializer):
     recipient_count = serializers.IntegerField()
     recipients = DetailRecipientSerializer(many=True)
-
-    class Meta:
-        fields = BaseSenderSerializer.Meta.fields + (
-            'recipient_count',
-            'recipients',
-        )
