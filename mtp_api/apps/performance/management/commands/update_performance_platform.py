36dbacfc53749bb884e5ec32c6176872f9ce806e
Ian Brechin <ian.brechin@gmail.com>
2016-08-25 16:50:20 +0100

    Add command to update performance platform data
    Allows for the creation of updaters, which function to update a particular
    resource on the govuk performance platform.

---
+++ mtp_api/apps/performance/management/commands/update_performance_platform.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from django.core.management import BaseCommand
+from django.utils import timezone
+
+from performance.updaters import registry
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--resources', nargs='*', default=list(registry.keys()),
+                            choices=list(registry.keys()),
+                            help='Resources to update.')
+        parser.add_argument('--timestamp', default=None,
+                            help='Timestamp in format Y-m-dTH:M:S')
+
+    def handle(self, *args, **options):
+        timestamp = (
+            timezone.make_aware(datetime.strptime(options['timestamp'], '%Y-%m-%dT%H:%M:%S'))
+            if options['timestamp'] else None
+        )
+        for resource in options['resources']:
+            for updater in registry[resource]:
+                updater(timestamp=timestamp).run()
