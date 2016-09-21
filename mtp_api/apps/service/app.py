ef819faf574bdf22714af5a294b5f5db9776a9ef
Ian Brechin <ian.brechin@gmail.com>
2016-09-21 17:20:50 +0100

    Create app to track downtimes of services
    This allows one to add a scheduled downtime for a service. The status
    of all services is returned via the healthcheck, and if the end time
    of the downtime is known then this is also included in the healthcheck
    response.

---
+++ mtp_api/apps/service/app.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig as DjangoAppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class AppConfig(DjangoAppConfig):
+    name = 'service'
+    verbose_name = _('Service availability')
---
+++ mtp_api/apps/service/tests/test_downtime.py
@@ -0,0 +1,98 @@
+from datetime import timedelta
+import json
+
+from django.test import TestCase
+from django.utils import timezone
+
+from service.models import Downtime, SERVICES
+
+
+class DowntimeTestCase(TestCase):
+
+    def _downtime_is_active(self, service, start, end):
+        downtime = Downtime(service=service, start=start, end=end)
+        downtime.save()
+
+        active_downtime = Downtime.objects.active_downtime(service)
+        self.assertIsNotNone(active_downtime)
+        return active_downtime
+
+    def _downtime_is_not_active(self, service, start, end):
+        downtime = Downtime(service=service, start=start, end=end)
+        downtime.save()
+
+        active_downtime = Downtime.objects.active_downtime(service)
+        self.assertIsNone(active_downtime)
+
+    def test_downtime_with_start_is_active_after_start(self):
+        one_hour_ago = timezone.now() - timedelta(hours=1)
+        self._downtime_is_active(SERVICES.GOV_UK_PAY, one_hour_ago, None)
+
+    def test_downtime_with_both_times_is_active_within_time_period(self):
+        one_hour_ago = timezone.now() - timedelta(hours=1)
+        in_one_hour = timezone.now() + timedelta(hours=1)
+        self._downtime_is_active(SERVICES.GOV_UK_PAY, one_hour_ago, in_one_hour)
+
+    def test_downtime_with_only_start_is_not_active_before_start(self):
+        in_one_hour = timezone.now() + timedelta(hours=1)
+        self._downtime_is_not_active(SERVICES.GOV_UK_PAY, in_one_hour, None)
+
+    def test_downtime_withboth_times_is_not_active_before_start(self):
+        in_one_hour = timezone.now() + timedelta(hours=1)
+        in_two_hours = timezone.now() + timedelta(hours=2)
+        self._downtime_is_not_active(SERVICES.GOV_UK_PAY, in_one_hour, in_two_hours)
+
+    def test_downtime_with_both_times_is_not_active_after_end(self):
+        two_hours_ago = timezone.now() - timedelta(hours=2)
+        one_hour_ago = timezone.now() - timedelta(hours=1)
+        self._downtime_is_not_active(SERVICES.GOV_UK_PAY, two_hours_ago, one_hour_ago)
+
+    def test_multiple_downtimes_shows_latest_active_end(self):
+        one_hour_ago = timezone.now() - timedelta(hours=1)
+        in_one_hour = timezone.now() + timedelta(hours=1)
+        earlier_downtime = Downtime(
+            service=SERVICES.GOV_UK_PAY, start=one_hour_ago, end=in_one_hour)
+        earlier_downtime.save()
+
+        in_two_hours = timezone.now() + timedelta(hours=2)
+        later_downtime = Downtime(
+            service=SERVICES.GOV_UK_PAY, start=one_hour_ago, end=in_two_hours)
+        later_downtime.save()
+
+        in_three_hours = timezone.now() + timedelta(hours=3)
+        later_inactive_downtime = Downtime(
+            service=SERVICES.GOV_UK_PAY, start=in_one_hour, end=in_three_hours)
+        later_inactive_downtime.save()
+
+        active_downtime = Downtime.objects.active_downtime(SERVICES.GOV_UK_PAY)
+        self.assertEqual(active_downtime.end, later_downtime.end)
+
+
+class DowntimeHealthcheckTestCase(TestCase):
+
+    def _get_healthcheck_data(self):
+        response = self.client.get('/healthcheck.json')
+        return json.loads(str(response.content, 'utf-8'))
+
+    def test_healthcheck_returns_active_downtime(self):
+        one_hour_ago = timezone.now() - timedelta(hours=1)
+        in_one_hour = timezone.now() + timedelta(hours=1)
+        downtime = Downtime(service=SERVICES.GOV_UK_PAY, start=one_hour_ago, end=in_one_hour)
+        downtime.save()
+
+        data = self._get_healthcheck_data()
+        self.assertFalse(data['gov_uk_pay']['status'])
+        self.assertEqual(data['gov_uk_pay']['downtime_end'], in_one_hour.isoformat())
+
+    def test_healthcheck_returns_active_downtime_with_no_end(self):
+        one_hour_ago = timezone.now() - timedelta(hours=1)
+        downtime = Downtime(service=SERVICES.GOV_UK_PAY, start=one_hour_ago, end=None)
+        downtime.save()
+
+        data = self._get_healthcheck_data()
+        self.assertFalse(data['gov_uk_pay']['status'])
+        self.assertNotIn('downtime_end', data['gov_uk_pay'])
+
+    def test_healthcheck_returns_no_downtime(self):
+        data = self._get_healthcheck_data()
+        self.assertTrue(data['gov_uk_pay']['status'])
