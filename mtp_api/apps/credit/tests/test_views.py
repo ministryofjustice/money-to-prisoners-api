472ec0d6744d2a91cf8341580b464f57ef54a0c4
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-10-07 12:13:26 +0100

    Ensure credit ordering test uses a filter that returns credits visible to test user

--- mtp_api/apps/credit/tests/test_views.py
+++ mtp_api/apps/credit/tests/test_views.py
@@ -731,6 +731,12 @@ class CreditListInvalidValuesTestCase(CreditListTestCase):
 
 
 class CreditListOrderingTestCase(CreditListTestCase):
+    transaction_batch = 200
+
+    def setUp(self):
+        super().setUp()
+        self.logged_in_user = self._get_authorised_user()
+
     @classmethod
     def add_test_methods(cls):
         for ordering_field in ['received_at', 'amount', 'prisoner_number', 'prisoner_name']:
@@ -746,18 +752,17 @@ class CreditListOrderingTestCase(CreditListTestCase):
             search = ''
             while not search:
                 credit = random.choice(self.credits)
-                if credit.prisoner_name:
+                if credit.prison in self.logged_in_user.prisonusermapping.prisons.all() and credit.prisoner_name:
                     search = credit.prisoner_name[:-4].strip()
-            self._test_ordering(ordering, prisoner_name=search)
+            self._test_ordering(ordering, prisoner_name=search.lower())
 
         setattr(cls, 'test_ordering_by_' + ordering, test_method)
 
     def _test_ordering(self, ordering, **filters):
-        logged_in_user = self._get_authorised_user()
         url = self._get_url(ordering=ordering, **filters)
         response = self.client.get(
             url, format='json',
-            HTTP_AUTHORIZATION=self.get_http_authorization_for_user(logged_in_user)
+            HTTP_AUTHORIZATION=self.get_http_authorization_for_user(self.logged_in_user)
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
