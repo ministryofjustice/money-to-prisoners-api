4b222ea6e7ff5d20b138944fef7a24073db64f14
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-11-23 12:25:59 +0000

    Fix tick charts for zero division and allow values other than query sets

--- mtp_api/apps/core/templatetags/tick_charts.py
+++ mtp_api/apps/core/templatetags/tick_charts.py
@@ -1,19 +1,13 @@
-import functools
+import datetime
 
 from django import template
 from django.core.exceptions import FieldError
 from django.db import models
 from django.utils.crypto import get_random_string
 
+from core import mean
+
 register = template.Library()
-
-
-def mean(iterable):
-    reduced = functools.reduce(lambda previous, value: (previous[0] + 1, previous[1] + value),
-                               iterable, (0, 0))
-    if reduced[0] == 0:
-        return float('nan')
-    return reduced[1] / reduced[0]
 
 
 def get_aggregates(values, stride_field, value_field):
@@ -23,6 +17,8 @@ def get_aggregates(values, stride_field, value_field):
                                 count=models.Count('*'))
     except (AttributeError, FieldError):
         values = list(values)
+        if not values:
+            return {'count': 0}
         return {
             'min_stride': values[0][stride_field],
             'max_stride': values[-1][stride_field],
@@ -32,10 +28,21 @@ def get_aggregates(values, stride_field, value_field):
         }
 
 
+def get_zero(value):
+    if isinstance(value, (datetime.date, datetime.datetime)):
+        return datetime.timedelta()
+    if isinstance(value, (int, float)):
+        return 0.0
+    raise ValueError('Cannot determine zero value for a %s' % type(value))
+
+
 @register.inclusion_tag('core/tick-chart.html')
-def tick_chart(queryset, stride_field, value_field, grouping_size=200, grouping_method='mean',
+def tick_chart(values, stride_field, value_field, grouping_size=200, grouping_method='mean', max_value=None,
                width=610, height=50, inset=2, axes=True, highlight_negative=True):
-    values = queryset.values(stride_field, value_field).order_by(stride_field)
+    if isinstance(values, models.QuerySet):
+        values = values.values(stride_field, value_field).order_by(stride_field)
+    else:
+        values = list(values)
     aggregates = get_aggregates(values, stride_field, value_field)
     if aggregates['count'] < 2:
         return {}
@@ -63,16 +70,18 @@ def tick_chart(queryset, stride_field, value_field, grouping_size=200, grouping_
         ]
         aggregates = get_aggregates(values, stride_field, value_field)
 
-    width = min(width, aggregates['count'] * 50)
-    inner_width = width - inset * 2
-    inner_height = height - inset * 2
+    if max_value is not None:
+        aggregates['max_value'] = max_value
 
+    zero_stride = get_zero(aggregates['min_stride'])
     stride_span = aggregates['max_stride'] - aggregates['min_stride']
-    min_value = min(type(aggregates['min_value'])(), aggregates['min_value'])
+    if stride_span == zero_stride:
+        return {}  # graph has no horizontal axis
+    zero_vaue = get_zero(aggregates['min_value'])
+    min_value = min(zero_vaue, aggregates['min_value'])
     value_span = aggregates['max_value'] - min_value
-
-    negative_height = -min_value / value_span * inner_height
-    positive_height = inner_height - negative_height
+    if value_span == zero_vaue:
+        value_span = aggregates['min_value']
 
     values = (
         {
@@ -81,6 +90,14 @@ def tick_chart(queryset, stride_field, value_field, grouping_size=200, grouping_
         }
         for value in values
     )
+
+    width = min(width, aggregates['count'] * 50)
+    inner_width = width - inset * 2
+    inner_height = height - inset * 2
+
+    negative_height = -min_value / value_span * inner_height
+    positive_height = inner_height - negative_height
+
     graph = (
         '%s%0.1f,%0.1f' % ('M' if index == 0 else 'L',
                            inset + value['stride'] * inner_width,
