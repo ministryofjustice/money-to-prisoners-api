667f752415455a3f644215d79fb6f80af8290f25
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-11-24 11:36:30 +0000

    Use plain-text 404 and 500 error responses instead of responses based on MTP-common and GOV.UK template

--- mtp_api/urls.py
+++ mtp_api/urls.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.http import HttpResponse
-from django.template.response import TemplateResponse
 from django.views.generic import RedirectView
+from django.utils.translation import gettext_lazy as _
 
 from moj_irat.views import HealthcheckView, PingJsonView
 
@@ -30,6 +30,8 @@ urlpatterns = [
     url(r'^favicon.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico', permanent=True)),
     url(r'^robots.txt$', lambda request: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')),
 
-    url(r'^404.html$', lambda request: TemplateResponse(request, 'mtp_common/errors/404.html', status=404)),
-    url(r'^500.html$', lambda request: TemplateResponse(request, 'mtp_common/errors/500.html', status=500)),
+    url(r'^404.html$', lambda request: HttpResponse(_('Page not found'),
+                                                    content_type='text/plain', status=404)),
+    url(r'^500.html$', lambda request: HttpResponse(_('Sorry, something went wrong'),
+                                                    content_type='text/plain', status=500)),
 ]
