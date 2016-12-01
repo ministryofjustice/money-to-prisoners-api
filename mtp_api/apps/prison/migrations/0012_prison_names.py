1941ea288c25f358d694fba35215c57e15ef4f73
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-12-01 11:26:25 +0000

    Rename prisons to use title case

---
+++ mtp_api/apps/prison/migrations/0012_prison_names.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+prefixes = ('HMP & YOI', 'HMP', 'HMYOI & RC', 'HMYOI', 'IRC')  # from Prison.re_prefixes
+name_sql = 'initcap(name)'
+name_sql = '''regexp_replace({name_sql}, ' ISIS$', ' ISIS', 'i')'''.format(name_sql=name_sql)
+for prefix in prefixes:
+    name_sql = '''regexp_replace({name_sql}, '^{prefix} ', '{prefix} ', 'i')'''.format(prefix=prefix,
+                                                                                       name_sql=name_sql)
+forward_sql = 'UPDATE prison_prison SET name={}'.format(name_sql)
+reverse_sql = 'UPDATE prison_prison SET name=upper(name)'
+
+
+class Migration(migrations.Migration):
+    dependencies = [('prison', '0011_prisonerlocation_active')]
+    operations = [migrations.RunSQL(sql=forward_sql, reverse_sql=reverse_sql)]
