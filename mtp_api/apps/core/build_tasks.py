d7c20604c863f660971732a6d30985d47f4c5470
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-12-06 11:04:14 +0000

    Add test mode server with generated data `run.py start --test-mode` is equivalent of `make testserve` from previous version

--- mtp_api/apps/core/build_tasks.py
+++ mtp_api/apps/core/build_tasks.py
@@ -1,5 +1,18 @@
+from django.core.management import call_command
 from mtp_common.build_tasks.executor import Context
 from mtp_common.build_tasks.tasks import tasks
+
+
+@tasks.register('build')
+def start(context: Context, port=8000, test_mode=False):
+    """
+    Starts a development server with test data
+    """
+    if not test_mode:
+        super_task = context.overidden_tasks[0]
+        return super_task(context=context, port=port)
+    context.setup_django()
+    return call_command('testserver', 'initial_groups', 'test_prisons', addrport='0:%s' % port, interactive=False)
 
 
 @tasks.register(hidden=True)
