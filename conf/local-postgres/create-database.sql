3bd3d6d789a42b03697a4377672f4570897cee83
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-12-13 13:30:20 +0000

    Fix docker-compose settings for running the app locally

---
+++ conf/local-postgres/create-database.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS mtp_api;
+CREATE DATABASE mtp_api WITH encoding='UTF8' template=template0;
--- docker/postgres/create-database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "****** CREATING VIS DATABASE ******"
-gosu postgres postgres --single <<- EOSQL
-   CREATE DATABASE mtp_api ENCODING 'UTF8';
-EOSQL
-echo ""
-echo "****** VIS DATABASE CREATED ******"
