ef981c5fba9e66c3f7d23514db0ec5da8265d767
Ian Brechin <ian.brechin@gmail.com>
2017-03-02 16:26:23 +0000

    Remove old security views as they are no longer used
    Dynamis sender/prisoner views have been replaced by those in
    the security app.

--- mtp_api/apps/credit/serializers.py
+++ mtp_api/apps/credit/serializers.py
@@ -17,9 +17,7 @@ class CreditedOnlyCreditSerializer(serializers.ModelSerializer):
 
 
 class IdsCreditSerializer(serializers.Serializer):
-    credit_ids = serializers.ListField(
-       child=serializers.IntegerField()
-    )
+    credit_ids = serializers.ListField(child=serializers.IntegerField())
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -118,38 +116,3 @@ class LockedCreditSerializer(CreditSerializer):
             'locked',
             'locked_at',
         )
-
-
-class BaseSenderSerializer(serializers.Serializer):
-    sender_name = serializers.CharField(required=False)
-    sender_sort_code = serializers.CharField(required=False)
-    sender_account_number = serializers.CharField(required=False)
-    sender_roll_number = serializers.CharField(required=False)
-
-
-class BasePrisonerSerializer(serializers.Serializer):
-    prisoner_number = serializers.CharField()
-    prisoner_name = serializers.CharField()
-    prison_id = serializers.CharField()
-    prison_name = serializers.CharField()
-    current_prison_name = serializers.CharField()
-
-
-class DetailSenderSerializer(BaseSenderSerializer):
-    credit_count = serializers.IntegerField()
-    credit_total = serializers.IntegerField()
-
-
-class DetailPrisonerSerializer(BasePrisonerSerializer):
-    credit_count = serializers.IntegerField()
-    credit_total = serializers.IntegerField()
-
-
-class SenderSerializer(DetailSenderSerializer):
-    prisoners = DetailPrisonerSerializer(many=True)
-    prisoner_count = serializers.IntegerField()
-
-
-class PrisonerSerializer(DetailPrisonerSerializer):
-    senders = DetailSenderSerializer(many=True)
-    sender_count = serializers.IntegerField()
