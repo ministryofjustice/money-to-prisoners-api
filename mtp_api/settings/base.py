c23e965cccf5d7803f18b8e6cd6a4639659989e1
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-03-16 12:37:01 +0000

    Remove rest framework’s browsing API as everything is OAuth2 protected anyway

--- mtp_api/settings/base.py
+++ mtp_api/settings/base.py
@@ -235,6 +235,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20
 }
