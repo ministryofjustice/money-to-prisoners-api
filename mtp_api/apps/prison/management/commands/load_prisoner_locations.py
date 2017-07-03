194bf4ffb57ff2e33507e84a776622218d50c29b
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-07-03 17:26:30 +0100

    Lower complexity of offender location loader and increase staleness prevention to 1 hour

--- mtp_api/apps/prison/management/commands/load_prisoner_locations.py
+++ mtp_api/apps/prison/management/commands/load_prisoner_locations.py
@@ -20,7 +20,15 @@ class Command(BaseCommand):
     help = __doc__.strip()
     excluded_nomis_ids = {'ZCH'}  # prison NOMIS ids which MTP ignores
     released_establishment_code = 'OUT'  # offender has been released
-    ignored_establishment_code = {'OUT', 'TRN'}  # not actual establishments
+    ignored_establishment_codes = {'OUT', 'TRN'}  # not actual establishments
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.verbosity = 1
+        self.modified_only = False
+        self.name_fields = ('given_name_1', 'given_name_2', 'given_name_3', 'surname')
+        self.whitespace = re.compile(r'\s+')
+        self.known_prisons = set()
 
     def add_arguments(self, parser):
         super().add_arguments(parser)
@@ -44,14 +52,12 @@ class Command(BaseCommand):
         ):
             raise CommandError('Missing Single Offender ID details')
 
-        verbosity = options.get('verbosity', 1)
-        modified_only = options['modified_only']
-        name_fields = ('given_name_1', 'given_name_2', 'given_name_3', 'surname')
-        whitespace = re.compile(r'\s+')
+        self.verbosity = options.get('verbosity', 1)
+        self.modified_only = options['modified_only']
 
-        known_prisons = set(Prison.objects.values_list('nomis_id', flat=True)) - self.excluded_nomis_ids
-        if verbosity:
-            self.stdout.write('%d known prisons' % len(known_prisons))
+        self.known_prisons = set(Prison.objects.values_list('nomis_id', flat=True)) - self.excluded_nomis_ids
+        if self.verbosity:
+            self.stdout.write('%d known prisons' % len(self.known_prisons))
             self.stdout.write('Starting Single Offender ID session')
         session = OAuth2Session(
             client=BackendApplicationClient(client_id=options['oauth_client'])
@@ -66,9 +72,9 @@ class Command(BaseCommand):
         query = {
             'per_page': options['page_size'],
         }
-        if modified_only and PrisonerLocation.objects.exists():
-            updated_after = PrisonerLocation.objects.latest().created - datetime.timedelta(minutes=5)
-            if verbosity > 1:
+        if self.modified_only and PrisonerLocation.objects.exists():
+            updated_after = PrisonerLocation.objects.latest().created - datetime.timedelta(hours=1)
+            if self.verbosity > 1:
                 self.stdout.write('Only loading offenders modified since %s' % updated_after)
             query['updated_after'] = updated_after.strftime('%Y-%m-%dT%H:%M:%S')
         page = 1
@@ -81,41 +87,9 @@ class Command(BaseCommand):
             prisoners = response.json()
             if not prisoners:
                 break
-            prisoner_locations = []
-            locations_to_delete = []
-            for prisoner in prisoners:
-                single_offender_id = prisoner['id']
-                prisoner_number = prisoner.get('noms_id')
-                if not prisoner_number:
-                    self.stderr.write('Offender %s has no prisoner number' % single_offender_id)
-                    continue
-                prison_id = prisoner.get('establishment_code')
-                if modified_only and prison_id == self.released_establishment_code:
-                    if verbosity > 1:
-                        self.stdout.write('Will delete location for %s' % prisoner_number)
-                    locations_to_delete.append(prisoner_number)
-                    continue
-                elif prison_id not in known_prisons:
-                    if verbosity > 1 and prison_id not in self.ignored_establishment_code:
-                        self.stdout.write('Unknown establishment code %s for %s' % (prison_id, prisoner_number))
-                    continue
-                prisoner_dob = parse_date(prisoner.get('date_of_birth') or '')
-                if not prisoner_dob:
-                    self.stderr.write('Offender %s (%s) has no date of birth' % (prisoner_number, single_offender_id))
-                    continue
-                prisoner_name = ' '.join(prisoner.get(field) or '' for field in name_fields)
-                prisoner_name = whitespace.sub(' ', prisoner_name)
-                prisoner_locations.append(
-                    PrisonerLocation(
-                        single_offender_id=single_offender_id,
-                        prisoner_number=prisoner_number,
-                        prison_id=prison_id,
-                        prisoner_dob=prisoner_dob,
-                        prisoner_name=prisoner_name,
-                    )
-                )
+            prisoner_locations, locations_to_delete = self.process_batch(prisoners)
             with atomic():
-                if modified_only:
+                if self.modified_only:
                     for prisoner_location in prisoner_locations:
                         prisoner_location.active = True
                         locations_to_delete.append(prisoner_location.prisoner_number)
@@ -124,12 +98,48 @@ class Command(BaseCommand):
             page += 1
             locations_updated += len(prisoner_locations)
 
-        if not modified_only:
+        if not self.modified_only:
             with atomic():
                 PrisonerLocation.objects.filter(active=True).delete()
                 PrisonerLocation.objects.filter(active=False).update(active=True)
 
         credit_prisons_need_updating.send(sender=PrisonerLocation)
 
-        if verbosity:
+        if self.verbosity:
             self.stdout.write('%d prisoner locations loaded' % locations_updated)
+
+    def process_batch(self, prisoners):
+        prisoner_locations = []
+        locations_to_delete = []
+        for prisoner in prisoners:
+            single_offender_id = prisoner['id']
+            prisoner_number = prisoner.get('noms_id')
+            if not prisoner_number:
+                self.stderr.write('Offender %s has no prisoner number' % single_offender_id)
+                continue
+            prison_id = prisoner.get('establishment_code')
+            if self.modified_only and prison_id == self.released_establishment_code:
+                if self.verbosity > 1:
+                    self.stdout.write('Will delete location for %s' % prisoner_number)
+                locations_to_delete.append(prisoner_number)
+                continue
+            elif prison_id not in self.known_prisons:
+                if self.verbosity > 1 and prison_id not in self.ignored_establishment_codes:
+                    self.stdout.write('Unknown establishment code %s for %s' % (prison_id, prisoner_number))
+                continue
+            prisoner_dob = parse_date(prisoner.get('date_of_birth') or '')
+            if not prisoner_dob:
+                self.stderr.write('Offender %s (%s) has no date of birth' % (prisoner_number, single_offender_id))
+                continue
+            prisoner_name = ' '.join(prisoner.get(field) or '' for field in self.name_fields)
+            prisoner_name = self.whitespace.sub(' ', prisoner_name)
+            prisoner_locations.append(
+                PrisonerLocation(
+                    single_offender_id=single_offender_id,
+                    prisoner_number=prisoner_number,
+                    prison_id=prison_id,
+                    prisoner_dob=prisoner_dob,
+                    prisoner_name=prisoner_name,
+                )
+            )
+        return prisoner_locations, locations_to_delete
