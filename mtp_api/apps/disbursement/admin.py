fdf55dd449df58e1b26203fbdbc58a0a3091780e
Ian Brechin <ian.brechin@gmail.com>
2017-11-24 17:24:32 +0000

    Map recipient views to url path

--- mtp_api/apps/disbursement/admin.py
+++ mtp_api/apps/disbursement/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
+from core.admin import add_short_description
 from disbursement.models import Disbursement, Recipient
 from transaction.utils import format_amount
 
@@ -11,10 +14,19 @@ class DisbursementAdmin(admin.ModelAdmin):
         'method'
     )
     date_hierarchy = 'created'
+    exclude = ('recipient',)
+    readonly_fields = ('recipient_link',)
 
     @classmethod
     def formatted_amount(cls, instance):
         return format_amount(instance.amount)
+
+    @add_short_description('recipient')
+    def recipient_link(self, instance):
+        link = reverse(
+            'admin:disbursement_recipient_change', args=(instance.recipient.pk,)
+        )
+        return format_html('<a href="{}">{}</a>', link, instance.recipient)
 
 
 @admin.register(Recipient)
