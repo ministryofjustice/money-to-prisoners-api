2eefcf7409a4d46ce7c76883edb3cf8dac3f0ea4
Katy600 <katherine.mccann@digital.justice.gov.uk>
2018-01-18 17:36:36 +0000

    Create pence to pounds method that converts total amount to pounds.

--- mtp_api/apps/the_dashboard/views.py
+++ mtp_api/apps/the_dashboard/views.py
@@ -16,6 +16,9 @@ class DashboardView(TemplateView):
     """
     template_name = 'the_dashboard/digital_take_up.html'
 
+
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         data = []
@@ -31,12 +34,22 @@ class DashboardView(TemplateView):
             queryset_credit = Credit.objects.filter(received_at__range=(start_of_month, end_of_month))
             queryset_credit_amount = Credit.objects.filter(received_at__range=(start_of_month, end_of_month)).aggregate(Sum('amount'))
 
+            def pence_to_pounds(amount):
+                if(amount != None):
+                    return amount/100
+
+            amount_transaction_to_pounds = pence_to_pounds(queryset_transaction_amount['amount__sum'])
+            amount_credit_to_pounds = pence_to_pounds(queryset_credit_amount['amount__sum'])
+
             data.append({
             'transaction_count': queryset_transaction.count(),
             'credit_count': queryset_credit.count(),
-            'queryset_credit_amount': queryset_credit_amount,
-            'queryset_transaction_amount': queryset_transaction_amount,
+            'queryset_credit_amount': amount_credit_to_pounds,
+            'queryset_transaction_amount': amount_transaction_to_pounds,
             })
+
+
+            # print(data[0]['queryset_transaction_amount'])
 
         return context
 
