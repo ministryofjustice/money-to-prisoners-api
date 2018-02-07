4d6adb8ef1868140c436328b70500fbe75beb23c
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-02-07 17:22:22 +0000

    Use "first instance" check when running clean-up because using the template-deploy-managed environment variable is unreliable the default action is to clean if status cannot be determined

--- mtp_api/apps/core/management/commands/clean_up.py
+++ mtp_api/apps/core/management/commands/clean_up.py
@@ -1,21 +1,23 @@
 import textwrap
 
-from django.conf import settings
 from django.core.management import BaseCommand, call_command
+from mtp_common.stack import StackException, is_first_instance
 
 
 class Command(BaseCommand):
     """
     Perform periodic clean-up removing expired DB models.
-    This is designed to run on only one instance in an auto-
-    scaling group.
+    This is designed to run on only one instance in an auto-scaling group.
     """
     help = textwrap.dedent(__doc__).strip()
 
     def handle(self, *args, **options):
-        verbosity = options.get('verbosity', 1)
-
-        if settings.RUN_CLEANUP_TASKS:
+        verbosity = options['verbosity']
+        try:
+            run_cleanup_tasks = is_first_instance()
+        except StackException:
+            run_cleanup_tasks = True
+        if run_cleanup_tasks:
             if verbosity:
                 self.stdout.write('Performing clean-up tasks')
             call_command('clearsessions', verbosity=verbosity)
@@ -23,4 +25,4 @@ class Command(BaseCommand):
             call_command('clear_password_change_requests', verbosity=verbosity)
             call_command('clear_abandoned_payments', age=7, verbosity=verbosity)
         elif verbosity:
-            self.stdout.write('Clean-up tasks disabled')
+            self.stdout.write('Clean-up tasks do not run on secondary instances')
