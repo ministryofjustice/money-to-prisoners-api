711a6f163e93c7b762f5f2b4f805f5e3dfff484c
Katy600 <katherine.mccann@digital.justice.gov.uk>
2018-03-05 16:05:32 +0000

    Simplify methods for creating the correct dates to just 2 methods. Replace count with assigning previous month to None as well and using an elif statement to assign it to the sencond months value

--- mtp_api/apps/the_dashboard/views.py
+++ mtp_api/apps/the_dashboard/views.py
@@ -61,93 +61,20 @@ def get_user_satisfaction():
     return yearly_satisfaction_percentage
 
 
-
-
-
-
-# def calculate_periods_of_time():
-#     tz = timezone.get_current_timezone()
-#     today = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
-#     weekday = today.weekday()
-
-#     start_delta = datetime.timedelta(days=weekday, weeks=1)
-#     start_of_week = today - start_delta
-#     end_delta = datetime.timedelta(days=weekday)
-#     end_of_week = today - end_delta
-#     year = today.year
-#     last_year = today.year -1
-#     month = today.month
-#     the_month = adjust_month_if_12(month)
-#     current_month = today.replace(month=the_month['this_month'], day=1)
-#     previous_month = today.replace(month=the_month['last_month'], day=1)
-#     start_of_current_month = today.replace(month=the_month['this_month'], day=1)
-#     start_of_next_month = today.replace(month=the_month['next_month'], day=1)
-#     start_of_current_month_last_year = today.replace(year=last_year, month=the_month['this_month'], day=1)
-#     start_of_next_month_last_year = today.replace(year=last_year, month=the_month['next_month'], day=1)
-#     start_of_current_year = today.replace(month=1, day=1)
-
-#     return {
-#         'tz': tz,
-#         'today': today,
-#         'weekday': weekday,
-#         'end_of_week': end_of_week,
-#         'start_of_week': start_of_week,
-#         'year_now': year,
-#         'last_year': last_year,
-#         'month': month,
-#         'current_month': current_month,
-#         'previous_month': previous_month,
-#         'start_of_current_month': start_of_current_month,
-#         'start_of_next_month': start_of_next_month,
-#         'start_of_current_month_last_year': start_of_current_month_last_year,
-#         'start_of_next_month_last_year':start_of_next_month_last_year,
-#         'start_of_current_year': start_of_current_year,
-#     }
-
-def month_cant_be_zero(month, year):
+def get_previous_month(month, year):
+    month -=1
     if month == 0:
         month = 12
         year -= 1
-    return (month, year)
+    return month, year
 
 
-def month_cant_be_thirteen(month, year):
+def get_next_month(month, year):
+    month += 1
     if month == 13:
         month = 1
         year += 1
-    else:
-        month
-    return (month, year)
-
-def get_this_year_this_month(month, year):
-    month, year = month_cant_be_thirteen(month, year)
-    return {'the_year':year, 'the_month': month}
-
-
-def get_this_year_last_month(month, year):
-    month -= 1
-    month, year = month_cant_be_zero(month, year)
-    return {'the_year':year, 'the_month': month}
-
-def get_this_year_this_month_in_for_loop(month, year):
-    return (month, year)
-
-def get_this_year_next_month(month, year):
-    month += 1
-    month, year = month_cant_be_thirteen(month, year)
-    return {'the_year':year, 'the_month': month}
-
-
-def get_last_year_this_month(month, year):
-    year -= 1
-    month, year = month_cant_be_thirteen(month, year)
-    return {'the_year':year, 'the_month': month}
-
-def get_last_year_next_month(month, year):
-    month += 1
-    year -= 1
-    month, year = month_cant_be_thirteen(month, year)
-    return {'the_year':year, 'the_month': month}
+    return month, year
 
 
 def error_percentage(error, total ):
@@ -164,13 +91,6 @@ def transaction_by_post(the_digital_take_up, digital_transactions_count):
         transaction_by_post = 0
 
     return transaction_by_post
-
-
-     # if digital_take_up:
-     #            transaction_by_post = (1 - digital_take_up) * total_number_of_digital_transactions_in_month
-     #        else:
-     #            transaction_by_post = 0
-
 
 class DashboardView(TemplateView):
     """
@@ -192,27 +112,22 @@ class DashboardView(TemplateView):
         end_delta = datetime.timedelta(days=weekday)
         end_of_week = today - end_delta
         year = today.year
+        last_year = year -1
         month = today.month
-        for_month = month
-        for_month += 1
+        last_month, last_months_year = get_previous_month(month, year)
+        next_month, next_months_year = get_next_month(month, year)
+        next_month_previous_year = next_months_year - 1
 
-        this_month_this_year = get_this_year_this_month(month, year)
-        last_month_this_year = get_this_year_last_month(month, year)
-        next_month_this_year = get_this_year_next_month(month, year)
-        this_month_last_year = get_last_year_this_month(month, year)
-        next_month_last_year = get_last_year_next_month(month, year)
-
-        start_of_current_month = today.replace(month= this_month_this_year['the_month'], year=this_month_this_year['the_year'], day=1)
-        start_of_previous_month = today.replace(month= last_month_this_year['the_month'], year=last_month_this_year['the_year'], day=1)
-        start_of_next_month = today.replace(month= next_month_this_year['the_month'], year=next_month_this_year['the_year'], day=1)
-        start_of_current_month_last_year = today.replace(year=this_month_last_year['the_year'], month=this_month_last_year['the_month'], day=1)
-        start_of_next_month_last_year = today.replace(year=next_month_last_year['the_year'], month=next_month_last_year['the_month'], day=1)
+        start_of_current_month = today.replace(month=month, year=year, day=1)
+        start_of_previous_month = today.replace(month=last_month, year=last_months_year, day=1)
+        start_of_next_month = today.replace(month=next_month, year=next_months_year, day=1)
+        start_of_current_month_last_year = today.replace(year=last_year, month=month, day=1)
+        start_of_next_month_last_year = today.replace(year=next_month_previous_year, month=next_month, day=1)
         start_of_current_year = today.replace(month=1, day=1)
 
         queryset_digital_transactions_this_month = Credit.objects.filter(received_at__range=(start_of_current_month, start_of_next_month))
         queryset_digital_transactions_this_year = Credit.objects.filter(received_at__range=(start_of_current_year, today))
         queryset_digital_transactions_previous_week = Credit.objects.filter(received_at__range=(start_of_week, end_of_week))
-
         queryset_digital_takeup = DigitalTakeup.objects.filter(date__range=(start_of_current_year, today))
 
         queryset_disbursements_this_year = Disbursement.objects.filter(created__range=(start_of_current_year, today))
@@ -239,9 +154,7 @@ class DashboardView(TemplateView):
         digital_transactions_amount_this_year = queryset_digital_transactions_this_year.aggregate(Sum('amount'))['amount__sum']
 
         digital_take_up_this_year = queryset_digital_takeup.mean_digital_takeup()
-        print("Digital take up",  digital_take_up_this_year)
         transaction_by_post_this_year = transaction_by_post( digital_take_up_this_year, digital_transactions_count_this_year)
-        print("TRANSACTION BY POST", transaction_by_post_this_year)
 
         transaction_by_digital = queryset_digital_transactions_this_year.filter(resolution=CREDIT_RESOLUTION.CREDITED).count()
         COST_PER_TRANSACTION_BY_POST = 5.73
@@ -283,43 +196,35 @@ class DashboardView(TemplateView):
 
 
         transaction_by_post_current_month = None
+        transaction_by_post_previous_month = None
         bank_transfer_count_current_month = None
+        bank_transfer_count_previous_month = None
         debit_card_count_current_month = None
+        debit_card_count_previous_month = None
         disbursement_amount_current_month = None
         disbursement_count_current_month = None
         digital_transactions_count_current_month = None
         digital_transactions_amount_current_month = None
 
         count = 0
+        start_month, year = get_next_month(month, year)
         for _ in range(5):
-            print("MONTH", for_month)
             count += 1
-            for_month -= 1
-
-            for_month, year = month_cant_be_zero(for_month, year)
-            for_month, year  = get_this_year_this_month_in_for_loop(for_month, year)
-            for_month, year = month_cant_be_thirteen(for_month, year)
-            next_month = for_month + 1
-            next_month, the_year = month_cant_be_thirteen(next_month, year)
+            start_month, the_year = start_month, year
+            start_month, year = get_previous_month(start_month, year)
+            next_month, the_year = get_next_month(start_month, year)
 
             end_of_month = datetime.datetime(year=the_year, month=next_month, day=1)
-            start_of_month = datetime.datetime(year=year, month=for_month, day=1)
+            start_of_month = datetime.datetime(year=year, month=start_month, day=1)
             start_of_month = tz.localize(start_of_month)
             end_of_month = tz.localize(end_of_month)
-
 
             takeup_queryset = DigitalTakeup.objects.filter(date__range=(start_of_month, end_of_month))
             queryset_total_number_of_digital_transactions_in_month = Credit.objects.filter(received_at__range=(start_of_month, end_of_month))
             total_number_of_digital_transactions_in_month = queryset_total_number_of_digital_transactions_in_month.count()
 
             digital_take_up = takeup_queryset.mean_digital_takeup()
-            # print(total_number_of_digital_transactions_in_month)
-            print("REAL DIGITAL TAKE UP", digital_take_up)
-            # digital_take_up = 0.9259259259259259
-            # print("FAKE DigitalTakeup", digital_take_up)
             transaction_by_post_by_month = transaction_by_post(digital_take_up, total_number_of_digital_transactions_in_month)
-            print("TRANSACTION BY POST BY MONTG", transaction_by_post_by_month)
-            # print("NUMBER OF DIGITAL TRANSACTION THIS MONTH", total_number_of_digital_transactions_in_month)
 
             bank_transfers = queryset_bank_transfer.filter(received_at__range=(start_of_month, end_of_month))
             debit_cards = queryset_debit_card.filter(received_at__range=(start_of_month, end_of_month))
@@ -343,19 +248,19 @@ class DashboardView(TemplateView):
             if transaction_by_post_current_month is None:
                 transaction_by_post_current_month = transaction_by_post_by_month
 
-            if count == 2:
+            elif transaction_by_post_previous_month is None:
                 transaction_by_post_previous_month = transaction_by_post_by_month
 
             if bank_transfer_count_current_month is None:
                 bank_transfer_count_current_month = bank_transfer_count
 
-            if count == 2:
+            elif bank_transfer_count_previous_month is None:
                 bank_transfer_count_previous_month = bank_transfer_count
 
             if debit_card_count_current_month is None:
                 debit_card_count_current_month = debit_card_count
 
-            if count == 2:
+            elif debit_card_count_previous_month is None:
                 debit_card_count_previous_month = debit_card_count
 
             if disbursement_amount_current_month is None:
