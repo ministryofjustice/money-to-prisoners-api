495a8a4dd4b4dca72d3ceba710bf559f7e5ca955
Katy600 <katherine.mccann@digital.justice.gov.uk>
2018-04-18 14:35:16 +0100

    Remove rebase errors

--- mtp_api/apps/the_dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-     url(r'^dashboard/$', views.DashboardView.as_view(), name='dashboard'),
-     # url(r'^dashboard_two/$', view_two.DashboardTwoView.as_view(), name='dashboard_two'),
-]
