b16dff7cc8b2bf50075a1b8e1b7b5bac54820201
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-11-27 18:12:25 +0000

    Notify admins when there are day-old pending account requests

---
+++ mtp_api/apps/mtp_auth/management/commands/send_account_request_emails.py
@@ -0,0 +1,69 @@
+import datetime
+import logging
+
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.core.management import BaseCommand
+from django.db import models
+from django.db.models.functions import Concat
+from django.utils.text import capfirst
+from django.utils.timezone import now
+from django.utils.translation import gettext
+from mtp_common.tasks import send_email
+
+from mtp_auth.models import AccountRequest, Role
+from prison.models import Prison
+
+logger = logging.getLogger('mtp')
+
+
+class Command(BaseCommand):
+    """
+    Emails user admins that have pending account requests
+
+    Sends an email to all admins for the user's team who have pending requests created
+    *any time yesterday*. This command should only be scheduled once a day.
+
+    TODO: only currently handles teams based on role AND prison, i.e. only applicable to cashbook self-sign-up
+    """
+    help = __doc__.strip().splitlines()[0]
+
+    def handle(self, *args, **options):
+        grouped_requests = AccountRequest.objects.filter(
+            created__date=now() - datetime.timedelta(days=1)
+        ).order_by().values('role', 'prison').annotate(
+            names=ArrayAgg(Concat('first_name', models.Value(' '), 'last_name'))
+        )
+        for group in grouped_requests:
+            prison = Prison.objects.get(pk=group['prison'])
+            role = Role.objects.get(pk=group['role'])
+            names = group['names']
+            admins = self.find_admins(role, prison)
+            if admins:
+                self.email_admins(admins, role, prison, names)
+            else:
+                logger.error('No active user admins for %s in %s' % (role.name, prison.name))
+
+    def find_admins(self, role, prison):
+        admins = role.key_group.user_set.filter(
+            is_active=True, is_superuser=False
+        ).filter(groups__name='UserAdmin').filter(prisonusermapping__prisons=prison)
+        return list(admins)
+
+    def email_admins(self, admins, role, prison, names):
+        service_name = role.application.name.lower()
+        prison_name = prison.short_name
+        send_email(
+            [admin.email for admin in admins],
+            'mtp_auth/new_account_requests.txt',
+            capfirst(gettext('You have new %(service_name)s users to approve') % {
+                'service_name': service_name,
+            }),
+            context={
+                'service_name': service_name,
+                'prison_name': prison_name,
+                'names': names,
+                'login_url': role.login_url,
+            },
+            html_template='mtp_auth/new_account_requests.html',
+            anymail_tags=['new-account-requests'],
+        )
