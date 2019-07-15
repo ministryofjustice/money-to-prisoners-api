0eed932ec3496d461b5d0a92521895dd0abe9a89
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-07-15 18:30:26 +0100

    Replace single periodic notification email with a daily one that comes in 3 flavours: - first batch of notifications - further batches of notifications - info email to encourage those not monitoring profiles to do so

--- mtp_api/apps/notification/management/commands/send_notification_emails.py
+++ mtp_api/apps/notification/management/commands/send_notification_emails.py
@@ -1,98 +1,153 @@
-from urllib.parse import urljoin
-
 from django.conf import settings
 from django.core.management import BaseCommand
-from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 from mtp_common.tasks import send_email
 
-from credit.models import Credit
-from disbursement.models import Disbursement
-from mtp_auth.models import PrisonUserMapping
 from notification.constants import EMAIL_FREQUENCY, get_notification_period
 from notification.models import Event, EmailNotificationPreferences
+from notification.rules import ENABLED_RULES
+from security.models import PrisonerProfile, BankAccount, DebitCardSenderDetails
+
+EMAILS_STARTED_FLAG = 'notifications-started'
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument('--frequency', default=EMAIL_FREQUENCY.WEEKLY,
-                            choices=EMAIL_FREQUENCY.values,
-                            help='Set which frequency the command is being run for')
-
     def handle(self, **options):
-        frequency = options['frequency']
-        preferences = EmailNotificationPreferences.objects.filter(frequency=frequency)
+        frequency = EMAIL_FREQUENCY.DAILY
         period_start, period_end = get_notification_period(frequency)
-        memo_table = {}
+        events = get_events(period_start, period_end)
 
-        if frequency == EMAIL_FREQUENCY.DAILY:
-            period = _('day')
-        elif frequency == EMAIL_FREQUENCY.WEEKLY:
-            period = _('week')
-        else:
-            period = _('month')
+        email_context = {
+            'period_start': period_start,
+            'notifications_url': (
+                f'{settings.NOMS_OPS_URL}/security/notifications/#date-{period_start.date().isoformat()}'
+            ),
+            'sender_url': f'{settings.NOMS_OPS_URL}/security/senders/',
+            'prisoner_url': f'{settings.NOMS_OPS_URL}/security/prisoners/',
+            'settings_url': f'{settings.NOMS_OPS_URL}/settings/',
+            'feedback_url': f'{settings.NOMS_OPS_URL}/feedback/',
+            'staff_email': True,
+        }
 
-        notifications_period_url = urljoin(
-            settings.NOMS_OPS_NOTIFICATIONS_URL,
-            '%s/' % period_start.date().isoformat()
-        )
-
+        preferences = EmailNotificationPreferences.objects.filter(frequency=frequency)
         for preference in preferences:
             user = preference.user
-            total_notifications = get_notification_count(
-                user, period_start, period_end, memo_table
-            )
+            event_group = group_events(events, user)
 
-            if total_notifications > 0:
-                email_context = {
-                    'user': user,
-                    'period': period,
-                    'period_start': period_start,
-                    'total_notifications': total_notifications,
-                    'notifications_url': notifications_period_url,
-                    'settings_url': settings.NOMS_OPS_SETTINGS_URL,
-                    'feedback_url': settings.NOMS_OPS_FEEDBACK_URL,
-                    'staff_email': True,
-                }
-                send_email(
-                    user.email, 'notification/periodic_email.txt',
-                    _('You have %s prisoner money intelligence tool notifications') % total_notifications,
-                    context=email_context,
-                    html_template='notification/periodic_email.html',
-                    anymail_tags=['intelligence-notifications'],
+            has_notifications = event_group['transaction_count']
+            is_monitoring = any((
+                model.objects.filter(monitoring_users=user).exists()
+                for model in (PrisonerProfile, DebitCardSenderDetails, BankAccount)
+            ))
+            emails_started = user.flags.filter(name=EMAILS_STARTED_FLAG).exists()
+
+            email_context['user'] = user
+            email_context['event_group'] = event_group
+            if emails_started and has_notifications:
+                send_email_with_events(email_context)
+            elif not emails_started:
+                if has_notifications:
+                    send_first_email_with_events(email_context)
+                    user.flags.create(name=EMAILS_STARTED_FLAG)
+                elif not is_monitoring:
+                    send_first_email_not_monitoring(email_context)
+                    user.flags.create(name=EMAILS_STARTED_FLAG)
+
+
+def get_events(period_start, period_end):
+    return Event.objects.filter(
+        rule__in=ENABLED_RULES,
+        triggered_at__gte=period_start, triggered_at__lt=period_end,
+    )
+
+
+def group_events(events, user):
+    senders = {}
+    prisoners = {}
+    for event in events.filter(user=user):
+        if hasattr(event, 'sender_profile_event'):
+            profile = event.sender_profile_event.sender_profile
+            if profile.id in senders:
+                details = senders[profile.id]
+            else:
+                details = make_date_group_profile(
+                    profile.id,
+                    profile.get_sorted_sender_names()[0]
                 )
+                senders[profile.id] = details
+            if hasattr(event, 'credit_event'):
+                details['credit_ids'].add(event.credit_event.credit_id)
+            if hasattr(event, 'disbursement_event'):
+                details['disbursement_ids'].add(event.disbursement_event.disbursement_id)
+
+        if hasattr(event, 'prisoner_profile_event'):
+            profile = event.prisoner_profile_event.prisoner_profile
+            if profile.id in prisoners:
+                details = prisoners[profile.id]
+            else:
+                details = make_date_group_profile(
+                    profile.id,
+                    f'{profile.prisoner_name} ({profile.prisoner_number})'
+                )
+                prisoners[profile.id] = details
+            if hasattr(event, 'credit_event'):
+                details['credit_ids'].add(event.credit_event.credit_id)
+            if hasattr(event, 'disbursement_event'):
+                details['disbursement_ids'].add(event.disbursement_event.disbursement_id)
+
+    senders = sorted(senders.values(), key=lambda sender: sender['description'])
+    prisoners = sorted(prisoners.values(), key=lambda prisoner: prisoner['description'])
+    transaction_count = 0
+    for profile in senders:
+        profile.pop('disbursement_ids')
+        profile['transaction_count'] = len(profile.pop('credit_ids'))
+        transaction_count += profile['transaction_count']
+    for profile in prisoners:
+        profile['disbursements_only'] = bool(profile['disbursement_ids'] and not profile['credit_ids'])
+        profile['transaction_count'] = len(profile.pop('credit_ids')) + len(profile.pop('disbursement_ids'))
+        transaction_count += profile['transaction_count']
+
+    return {
+        'senders': senders,
+        'prisoners': prisoners,
+        'transaction_count': transaction_count,
+    }
 
 
-def get_notification_count(user, period_start, period_end, memo_table=None):
-    prisons = PrisonUserMapping.objects.get_prison_set_for_user(user)
-    key = '.'.join(sorted(list(prisons.values_list('pk', flat=True))))
+def make_date_group_profile(profile_id, description):
+    return {
+        'id': profile_id,
+        'description': description,
+        'credit_ids': set(),
+        'disbursement_ids': set(),
+    }
 
-    if memo_table is not None and key in memo_table:
-        total_events = memo_table[key]
-    else:
-        prison_filter = Q()
-        if key:
-            prison_filter |= (
-                Q(credit_event__credit__prison__in=prisons) |
-                Q(disbursement_event__disbursement__prison__in=prisons)
-            )
 
-        events = Event.objects.filter(
-            prison_filter,
-            triggered_at__gte=period_start, triggered_at__lt=period_end
-        )
-        total_events = events.filter(
-            rule__in=['NWN', 'HA']
-        ).count()
-        if memo_table is not None:
-            memo_table[key] = total_events
+def send_email_with_events(email_context):
+    send_email(
+        email_context['user'].email, 'notification/notifications.txt',
+        _('Your new intelligence tool notifications'),
+        context=email_context,
+        html_template='notification/notifications.html',
+        anymail_tags=['intel-notification', 'intel-notification-daily'],
+    )
 
-    monitored_credits = Credit.objects.filter(
-        received_at__gte=period_start, received_at__lt=period_end
-    ).get_monitored_credits(user)
-    monitored_disbursements = Disbursement.objects.filter(
-        created__gte=period_start, created__lt=period_end
-    ).get_monitored_disbursements(user)
-    monitored_count = monitored_credits.count() + monitored_disbursements.count()
 
-    return total_events + monitored_count
+def send_first_email_with_events(email_context):
+    send_email(
+        email_context['user'].email, 'notification/notifications-first.txt',
+        _('New notification feature added to intelligence tool'),
+        context=email_context,
+        html_template='notification/notifications-first.html',
+        anymail_tags=['intel-notification', 'intel-notification-first'],
+    )
+
+
+def send_first_email_not_monitoring(email_context):
+    send_email(
+        email_context['user'].email, 'notification/not-monitoring.txt',
+        _('New helpful ways to get the best from the intelligence tool'),
+        context=email_context,
+        html_template='notification/not-monitoring.html',
+        anymail_tags=['intel-notification', 'intel-notification-not-monitoring'],
+    )
--- mtp_api/apps/notification/tests/test_commands.py
+++ mtp_api/apps/notification/tests/test_commands.py
@@ -1,18 +1,24 @@
+import datetime
+
 from django.core import mail
 from django.core.management import call_command
 from django.test import TestCase, override_settings
+from django.utils import timezone
+from model_mommy import mommy
 
 from core.tests.utils import make_test_users
+from credit.constants import CREDIT_RESOLUTION
+from credit.models import Credit
+from disbursement.models import Disbursement
 from disbursement.tests.utils import generate_disbursements
-from mtp_auth.models import PrisonUserMapping
 from notification.constants import EMAIL_FREQUENCY
-from notification.constants import get_notification_period
-from notification.management.commands.send_notification_emails import (
-    get_notification_count
-)
+from notification.management.commands.send_notification_emails import get_events, group_events, EMAILS_STARTED_FLAG
 from notification.models import Event, EmailNotificationPreferences
+from payment.models import Payment
 from payment.tests.utils import generate_payments
+from prison.models import PrisonerLocation
 from prison.tests.utils import load_random_prisoner_locations
+from security.models import PrisonerProfile, SenderProfile, DebitCardSenderDetails
 
 
 class SendNotificationEmailsTestCase(TestCase):
@@ -23,64 +29,137 @@ class SendNotificationEmailsTestCase(TestCase):
         test_users = make_test_users()
         self.security_staff = test_users['security_staff']
         load_random_prisoner_locations()
-        generate_payments(payment_batch=200, days_of_history=3)
-        generate_disbursements(disbursement_batch=200, days_of_history=3)
-
-    @override_settings(ENVIRONMENT='prod')
-    def test_send_email_notifications(self):
-        call_command('update_security_profiles')
-        EmailNotificationPreferences(
-            user=self.security_staff[0], frequency=EMAIL_FREQUENCY.DAILY
-        ).save()
-        call_command('send_notification_emails', frequency=EMAIL_FREQUENCY.DAILY)
-
-        self.assertEqual(len(mail.outbox), 1)
+        generate_payments(payment_batch=20, days_of_history=2)
+        generate_disbursements(disbursement_batch=20, days_of_history=2)
 
     @override_settings(ENVIRONMENT='prod')
     def test_does_not_send_email_notifications_for_no_events(self):
-        EmailNotificationPreferences(
-            user=self.security_staff[0], frequency=EMAIL_FREQUENCY.DAILY
-        ).save()
-        call_command('send_notification_emails', frequency=EMAIL_FREQUENCY.DAILY)
+        user = self.security_staff[0]
+        user.flags.create(name=EMAILS_STARTED_FLAG)
+        EmailNotificationPreferences(user=user, frequency=EMAIL_FREQUENCY.DAILY).save()
+        call_command('send_notification_emails')
 
+        self.assertFalse(Event.objects.exists())
         self.assertEqual(len(mail.outbox), 0)
 
-    def _check_notification_count(self, total_notifications, events):
-        total_count = 0
-        for event in events:
-            if event.rule in ['NWN', 'HA']:
-                total_count += 1
-
-        self.assertEqual(total_notifications, total_count)
-
-    def test_notification_count_for_national_security(self):
-        call_command('update_security_profiles')
+    @override_settings(ENVIRONMENT='prod')
+    def test_does_not_send_email_notifications_for_no_monitoring(self):
         user = self.security_staff[0]
-        period_start, period_end = get_notification_period(EMAIL_FREQUENCY.DAILY)
-        total_notifications = get_notification_count(user, period_start, period_end)
-        events = Event.objects.filter(
-            triggered_at__gte=period_start, triggered_at__lt=period_end
-        )
-        self._check_notification_count(total_notifications, events)
-
-    def test_notification_count_for_prison_security(self):
+        user.flags.create(name=EMAILS_STARTED_FLAG)
+        EmailNotificationPreferences(user=user, frequency=EMAIL_FREQUENCY.DAILY).save()
         call_command('update_security_profiles')
-        user = self.security_staff[1]
-        period_start, period_end = get_notification_period(EMAIL_FREQUENCY.DAILY)
-        total_notifications = get_notification_count(user, period_start, period_end)
+        call_command('send_notification_emails')
 
-        prisons = PrisonUserMapping.objects.get_prison_set_for_user(user)
-        credit_events = Event.objects.filter(
-            credit_event__isnull=False,
-            credit_event__credit__prison__in=prisons,
-            triggered_at__gte=period_start, triggered_at__lt=period_end
+        self.assertFalse(Event.objects.exists())
+        self.assertEqual(len(mail.outbox), 0)
+
+    @override_settings(ENVIRONMENT='prod')
+    def test_sends_first_email_not_monitoring(self):
+        user = self.security_staff[0]
+        EmailNotificationPreferences(user=user, frequency=EMAIL_FREQUENCY.DAILY).save()
+        call_command('update_security_profiles')
+        call_command('send_notification_emails')
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[-1].subject, 'New helpful ways to get the best from the intelligence tool')
+        self.assertTrue(user.flags.filter(name=EMAILS_STARTED_FLAG).exists())
+
+    def create_profiles_but_unlink_objects(self):
+        call_command('update_security_profiles')
+        Credit.objects.update(sender_profile=None, prisoner_profile=None)
+        Disbursement.objects.update(recipient_profile=None, prisoner_profile=None)
+        # NB: profiles will have incorrect counts and totals
+
+    @override_settings(ENVIRONMENT='prod')
+    def test_sends_first_email_with_events(self):
+        user = self.security_staff[0]
+        EmailNotificationPreferences(user=user, frequency=EMAIL_FREQUENCY.DAILY).save()
+        self.create_profiles_but_unlink_objects()
+        for profile in PrisonerProfile.objects.all():
+            profile.monitoring_users.add(user)
+        call_command('update_security_profiles')
+        call_command('send_notification_emails')
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[-1].subject, 'New notification feature added to intelligence tool')
+        self.assertTrue(user.flags.filter(name=EMAILS_STARTED_FLAG).exists())
+        yesterday = timezone.now() - datetime.timedelta(days=1)
+        yesterday = yesterday.date()
+        transaction_count = Event.objects.filter(triggered_at__date=yesterday, user=user).count()
+        self.assertIn(f'You have {transaction_count} notification', mail.outbox[-1].body)
+
+    @override_settings(ENVIRONMENT='prod')
+    def test_sends_subsequent_email_with_events(self):
+        user = self.security_staff[0]
+        user.flags.create(name=EMAILS_STARTED_FLAG)
+        EmailNotificationPreferences(user=user, frequency=EMAIL_FREQUENCY.DAILY).save()
+        self.create_profiles_but_unlink_objects()
+        for profile in DebitCardSenderDetails.objects.all():
+            profile.monitoring_users.add(user)
+        call_command('update_security_profiles')
+        call_command('send_notification_emails')
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[-1].subject, 'Your new intelligence tool notifications')
+        self.assertTrue(user.flags.filter(name=EMAILS_STARTED_FLAG).exists())
+        yesterday = timezone.now() - datetime.timedelta(days=1)
+        yesterday = yesterday.date()
+        transaction_count = Event.objects.filter(triggered_at__date=yesterday, user=user).count()
+        self.assertIn(f'You have {transaction_count} notification', mail.outbox[-1].body)
+
+    def test_profile_grouping(self):
+        user = self.security_staff[0]
+        call_command('update_security_profiles')
+        for profile in PrisonerProfile.objects.all():
+            profile.monitoring_users.add(user)
+        for profile in DebitCardSenderDetails.objects.all():
+            profile.monitoring_users.add(user)
+
+        period_start = timezone.make_aware(datetime.datetime.combine(timezone.now(), datetime.time.min))
+        period_start -= datetime.timedelta(days=7)
+        period_end = period_start + datetime.timedelta(days=1)
+        events = get_events(period_start, period_end)
+        self.assertFalse(events.exists())
+
+        prisoner_profile_1, prisoner_profile_2 = PrisonerProfile.objects.all()[:2]
+        sender_profile_1, sender_profile_2 = SenderProfile.objects.filter(debit_card_details__isnull=False)[:2]
+        debit_card_1 = sender_profile_1.debit_card_details.first()
+        debit_card_2 = sender_profile_2.debit_card_details.first()
+        credit = mommy.make(
+            Credit,
+            received_at=period_start, amount=100,
+            prisoner_number=prisoner_profile_1.prisoner_number, prisoner_name=prisoner_profile_1.prisoner_name,
+            prison=PrisonerLocation.objects.get(prisoner_number=prisoner_profile_1.prisoner_number).prison,
+            resolution=CREDIT_RESOLUTION.CREDITED, reconciled=True, private_estate_batch=None,
+            prisoner_profile=None, sender_profile=None,
         )
-        disbursement_events = Event.objects.filter(
-            disbursement_event__isnull=False,
-            disbursement_event__disbursement__prison__in=prisons,
-            triggered_at__gte=period_start, triggered_at__lt=period_end
+        mommy.make(
+            Payment,
+            credit=credit,
+            card_number_last_digits=debit_card_1.card_number_last_digits,
+            card_expiry_date=debit_card_1.card_expiry_date,
+            billing_address=debit_card_1.billing_addresses.first(),
         )
-        self._check_notification_count(
-            total_notifications,
-            credit_events.union(disbursement_events)
+        credit = mommy.make(
+            Credit,
+            received_at=period_start, amount=200,
+            prisoner_number=prisoner_profile_2.prisoner_number, prisoner_name=prisoner_profile_2.prisoner_name,
+            prison=PrisonerLocation.objects.get(prisoner_number=prisoner_profile_2.prisoner_number).prison,
+            resolution=CREDIT_RESOLUTION.CREDITED, reconciled=True, private_estate_batch=None,
+            prisoner_profile=None, sender_profile=None,
         )
+        mommy.make(
+            Payment,
+            credit=credit,
+            card_number_last_digits=debit_card_2.card_number_last_digits,
+            card_expiry_date=debit_card_2.card_expiry_date,
+            billing_address=debit_card_2.billing_addresses.first(),
+        )
+
+        call_command('update_security_profiles')
+
+        events = get_events(period_start, period_end)
+        event_group = group_events(events, user)
+        self.assertEqual(event_group['transaction_count'], 4)
+        self.assertEqual(len(event_group['senders']), 2)
+        self.assertEqual(len(event_group['prisoners']), 2)
