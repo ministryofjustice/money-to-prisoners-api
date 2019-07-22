e5c508774eb69edd95d629cb17de2218362b8293
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-07-22 17:28:31 +0100

    Make notification rule monitor tests more stable by ensuring random data definitely contains a subset that will trigger rules

--- mtp_api/apps/notification/tests/test_rules.py
+++ mtp_api/apps/notification/tests/test_rules.py
@@ -132,124 +132,107 @@ class RuleTestCase(TestCase):
             self.assertFalse(RULES['HA'].triggered(disbursement))
         self.assertEqual(Event.objects.count(), 20)
 
-    def test_create_events_for_monp(self):
+    def test_create_events_for_monp_credit(self):
         call_command('update_security_profiles')
 
-        prisoner_profile = PrisonerProfile.objects.filter(
-            credits__isnull=False,
-            disbursements__isnull=False
-        ).first()
-
+        prisoner_profile = PrisonerProfile.objects.filter(credits__isnull=False).first()
         prisoner_profile.monitoring_users.add(self.user)
 
-        credits = Credit.objects.filter(
-            prisoner_profile=prisoner_profile
-        )
-        disbursements = Disbursement.objects.filter(
-            prisoner_profile=prisoner_profile
-        )
-
+        credits = Credit.objects.filter(prisoner_profile=prisoner_profile)
         for credit in credits:
             self.assertTrue(RULES['MONP'].triggered(credit))
             events = RULES['MONP'].create_events(credit)
             self.assertEventMatchesRecord(events, credit, PrisonerProfile)
+
+        credits = Credit.objects.exclude(prisoner_profile=prisoner_profile)
+        for credit in credits:
+            self.assertFalse(RULES['MONP'].triggered(credit))
+
+        self.assertEqual(Event.objects.count(), prisoner_profile.credits.count())
+
+    def test_create_events_for_monp_disbursement(self):
+        call_command('update_security_profiles')
+
+        prisoner_profile = PrisonerProfile.objects.filter(disbursements__isnull=False).first()
+        prisoner_profile.monitoring_users.add(self.user)
+
+        disbursements = Disbursement.objects.filter(prisoner_profile=prisoner_profile)
         for disbursement in disbursements:
             self.assertTrue(RULES['MONP'].triggered(disbursement))
             events = RULES['MONP'].create_events(disbursement)
             self.assertEventMatchesRecord(events, disbursement, PrisonerProfile)
 
-        credits = Credit.objects.exclude(
-            prisoner_profile=prisoner_profile
-        )
-        disbursements = Disbursement.objects.exclude(
-            prisoner_profile=prisoner_profile
-        )
-
-        for credit in credits:
-            self.assertFalse(RULES['MONP'].triggered(credit))
+        disbursements = Disbursement.objects.exclude(prisoner_profile=prisoner_profile)
         for disbursement in disbursements:
             self.assertFalse(RULES['MONP'].triggered(disbursement))
+
+        self.assertEqual(Event.objects.count(), prisoner_profile.disbursements.count())
 
     def test_create_events_for_mons_debit_card(self):
         call_command('update_security_profiles')
 
         sender_profile = SenderProfile.objects.filter(
             credits__isnull=False,
-            debit_card_details__isnull=False
+            debit_card_details__isnull=False,
         ).first()
-
         sender_profile.debit_card_details.first().monitoring_users.add(
             self.user
         )
 
-        credits = Credit.objects.filter(
-            sender_profile=sender_profile
-        )
-
+        credits = Credit.objects.filter(sender_profile=sender_profile)
         for credit in credits:
             self.assertTrue(RULES['MONS'].triggered(credit))
             events = RULES['MONS'].create_events(credit)
             self.assertEventMatchesRecord(events, credit, SenderProfile)
 
-        credits = Credit.objects.exclude(
-            sender_profile=sender_profile
-        )
-
+        credits = Credit.objects.exclude(sender_profile=sender_profile)
         for credit in credits:
             self.assertFalse(RULES['MONS'].triggered(credit))
+
+        self.assertEqual(Event.objects.count(), sender_profile.credits.count())
 
     def test_create_events_for_mons_bank_account(self):
         call_command('update_security_profiles')
 
         sender_profile = SenderProfile.objects.filter(
             credits__isnull=False,
-            bank_transfer_details__isnull=False
+            bank_transfer_details__isnull=False,
         ).first()
-
         sender_profile.bank_transfer_details.first().sender_bank_account.monitoring_users.add(
             self.user
         )
 
-        credits = Credit.objects.filter(
-            sender_profile=sender_profile
-        )
-
+        credits = Credit.objects.filter(sender_profile=sender_profile)
         for credit in credits:
             self.assertTrue(RULES['MONS'].triggered(credit))
             events = RULES['MONS'].create_events(credit)
             self.assertEventMatchesRecord(events, credit, SenderProfile)
 
-        credits = Credit.objects.exclude(
-            sender_profile=sender_profile
-        )
-
+        credits = Credit.objects.exclude(sender_profile=sender_profile)
         for credit in credits:
             self.assertFalse(RULES['MONS'].triggered(credit))
+
+        self.assertEqual(Event.objects.count(), sender_profile.credits.count())
 
     def test_create_events_for_monr(self):
         call_command('update_security_profiles')
 
         recipient_profile = RecipientProfile.objects.filter(
             disbursements__isnull=False,
-            bank_transfer_details__isnull=False
+            bank_transfer_details__isnull=False,
         ).first()
-
         recipient_profile.bank_transfer_details.first().recipient_bank_account.monitoring_users.add(
             self.user
         )
 
-        disbursements = Disbursement.objects.filter(
-            recipient_profile=recipient_profile
-        )
-
+        disbursements = Disbursement.objects.filter(recipient_profile=recipient_profile)
         for disbursement in disbursements:
             self.assertTrue(RULES['MONR'].triggered(disbursement))
             events = RULES['MONR'].create_events(disbursement)
             self.assertEventMatchesRecord(events, disbursement, RecipientProfile)
 
-        disbursements = Disbursement.objects.exclude(
-            recipient_profile=recipient_profile
-        )
-
+        disbursements = Disbursement.objects.exclude(recipient_profile=recipient_profile)
         for disbursement in disbursements:
             self.assertFalse(RULES['MONR'].triggered(disbursement))
+
+        self.assertEqual(Event.objects.count(), recipient_profile.disbursements.count())
