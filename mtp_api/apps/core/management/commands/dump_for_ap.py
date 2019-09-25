1cfe6bf4a4ea9ad964e007167f8a3389c5268272
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-09-25 17:12:39 +0100

    Make dump command for analytical platform more like notification report creator

--- mtp_api/apps/core/management/commands/dump_for_ap.py
+++ mtp_api/apps/core/management/commands/dump_for_ap.py
@@ -25,7 +25,7 @@ class Command(BaseCommand):
         super().add_arguments(parser)
         parser.add_argument('--since', help='Since date (inclusive)')
         parser.add_argument('--until', help='Until date (exclusive)')
-        parser.add_argument('type', choices=['credits', 'disbursements'], help='Type of object to dump')
+        parser.add_argument('type', choices=list(Serialiser.serialisers), help='Type of object to dump')
         parser.add_argument('path', type=argparse.FileType('wt'), help='Path to dump data to')
 
     def handle(self, *args, **options):
@@ -34,14 +34,32 @@ class Command(BaseCommand):
         if since and until and until <= since:
             raise CommandError('"--until" must be after "--since"')
 
-        object_type = options['type']
-        writer = csv.DictWriter(options['path'], getattr(self, f'headers_{object_type}'))
+        record_type = options['type']
+        serialiser: Serialiser = Serialiser.serialisers[record_type]()
+        writer = csv.DictWriter(options['path'], serialiser.headers)
         writer.writeheader()
-        method = getattr(self, f'handle_{object_type}')
-        for row in method(since, until):
-            writer.writerow(row)
+        records = serialiser.get_records(since, until)
+        for record in records:
+            writer.writerow(serialiser.serialise(record))
 
-    headers_credits = [
+
+class Serialiser:
+    serialisers = {}
+    headers = []
+
+    def __init_subclass__(cls, serialised_model):
+        record_type = str(serialised_model._meta.verbose_name_plural)
+        cls.serialisers[record_type] = cls
+
+    def get_records(self, since, until):
+        raise NotImplementedError
+
+    def serialise(self, record):
+        raise NotImplementedError
+
+
+class CreditSerialiser(Serialiser, serialised_model=Credit):
+    headers = Serialiser.headers + [
         'Internal ID', 'URL',
         'Date received', 'Date credited',
         'Amount',
@@ -54,40 +72,39 @@ class Command(BaseCommand):
         'NOMIS transaction',
     ]
 
-    def handle_credits(self, since, until):
+    def get_records(self, since, until):
         if not since:
             since = Credit.objects.earliest().received_at
         if not until:
             until = Credit.objects.latest().received_at + datetime.timedelta(days=1)
             until.replace(hour=0, minute=0, second=0, microsecond=0)
-
-        records = Credit.objects.filter(
+        return Credit.objects.filter(
             received_at__gte=since,
             received_at__lt=until,
         ).order_by('pk').iterator(chunk_size=1000)
 
-        for record in records:
-            status = record.status
-            if status:
-                status = CREDIT_STATUS.for_value(status).display
-            else:
-                status = 'Anonymous'
-            row = {
-                'Internal ID': record.id,
-                'URL': f'{settings.NOMS_OPS_URL}/security/credits/{record.id}/',
-                'Date received': record.received_at,
-                'Date credited': record.log_set.get_action_date(CREDIT_LOG_ACTIONS.CREDITED),
-                'Amount': format_amount(record.amount),
-                'Prisoner number': record.prisoner_number or 'Unknown',
-                'Prisoner name': record.prisoner_name or 'Unknown',
-                'Prison': record.prison.short_name if record.prison else 'Unknown',
-                'Status': status,
-                'NOMIS transaction': record.nomis_transaction_id,
-            }
-            row.update(self.handle_credit_sender(record))
-            yield row
+    def serialise(self, record: Credit):
+        status = record.status
+        if status:
+            status = CREDIT_STATUS.for_value(status).display
+        else:
+            status = 'Anonymous'
+        row = {
+            'Internal ID': record.id,
+            'URL': f'{settings.NOMS_OPS_URL}/security/credits/{record.id}/',
+            'Date received': record.received_at,
+            'Date credited': record.log_set.get_action_date(CREDIT_LOG_ACTIONS.CREDITED),
+            'Amount': format_amount(record.amount),
+            'Prisoner number': record.prisoner_number or 'Unknown',
+            'Prisoner name': record.prisoner_name or 'Unknown',
+            'Prison': record.prison.short_name if record.prison else 'Unknown',
+            'Status': status,
+            'NOMIS transaction': record.nomis_transaction_id,
+        }
+        row.update(self.serialise_sender(record))
+        return row
 
-    def handle_credit_sender(self, record: Credit):
+    def serialise_sender(self, record: Credit):
         if hasattr(record, 'transaction'):
             transaction = record.transaction
             return {
@@ -115,7 +132,9 @@ class Command(BaseCommand):
             'Sender name': '(Unknown sender)',
         }
 
-    headers_disbursements = [
+
+class DisbursementSerialiser(Serialiser, serialised_model=Disbursement):
+    headers = Serialiser.headers + [
         'Internal ID', 'URL',
         'Date entered', 'Date confirmed', 'Date sent',
         'Amount',
@@ -127,40 +146,40 @@ class Command(BaseCommand):
         'NOMIS transaction', 'SOP invoice number',
     ]
 
-    def handle_disbursements(self, since, until):
+    def get_records(self, since, until):
         if not since:
             since = Disbursement.objects.earliest().created
         if not until:
             until = Disbursement.objects.latest().created + datetime.timedelta(days=1)
             until.replace(hour=0, minute=0, second=0, microsecond=0)
 
-        records = Disbursement.objects.filter(
+        return Disbursement.objects.filter(
             created__gte=since,
             created__lt=until,
         ).order_by('pk').iterator(chunk_size=1000)
 
-        for record in records:
-            yield {
-                'Internal ID': record.id,
-                'URL': f'{settings.NOMS_OPS_URL}/security/disbursements/{record.id}/',
-                'Date entered': record.created,
-                'Date confirmed': record.log_set.get_action_date(DISBURSEMENT_LOG_ACTIONS.CONFIRMED),
-                'Date sent': record.log_set.get_action_date(DISBURSEMENT_LOG_ACTIONS.SENT),
-                'Amount': format_amount(record.amount),
-                'Prisoner number': record.prisoner_number,
-                'Prisoner name': record.prisoner_name,
-                'Prison': record.prison.short_name,
-                'Recipient name': record.recipient_name,
-                'Payment method': DISBURSEMENT_METHOD.for_value(record.method).display,
-                'Bank transfer sort code': record.sort_code,
-                'Bank transfer account': record.account_number,
-                'Bank transfer roll number': record.roll_number,
-                'Recipient address': record.recipient_address,
-                'Recipient email': record.recipient_email,
-                'Status': DISBURSEMENT_RESOLUTION.for_value(record.resolution).display,
-                'NOMIS transaction': record.nomis_transaction_id,
-                'SOP invoice number': record.invoice_number,
-            }
+    def serialise(self, record: Disbursement):
+        return {
+            'Internal ID': record.id,
+            'URL': f'{settings.NOMS_OPS_URL}/security/disbursements/{record.id}/',
+            'Date entered': record.created,
+            'Date confirmed': record.log_set.get_action_date(DISBURSEMENT_LOG_ACTIONS.CONFIRMED),
+            'Date sent': record.log_set.get_action_date(DISBURSEMENT_LOG_ACTIONS.SENT),
+            'Amount': format_amount(record.amount),
+            'Prisoner number': record.prisoner_number,
+            'Prisoner name': record.prisoner_name,
+            'Prison': record.prison.short_name,
+            'Recipient name': record.recipient_name,
+            'Payment method': DISBURSEMENT_METHOD.for_value(record.method).display,
+            'Bank transfer sort code': record.sort_code,
+            'Bank transfer account': record.account_number,
+            'Bank transfer roll number': record.roll_number,
+            'Recipient address': record.recipient_address,
+            'Recipient email': record.recipient_email,
+            'Status': DISBURSEMENT_RESOLUTION.for_value(record.resolution).display,
+            'NOMIS transaction': record.nomis_transaction_id,
+            'SOP invoice number': record.invoice_number,
+        }
 
 
 def date_argument(argument):
