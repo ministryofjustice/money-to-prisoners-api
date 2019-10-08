2a542965e7184f24c7bc5684907e2ccd778c22eb
Marco Fucci <info@marcofucci.com>
2019-10-08 16:43:48 +0100

    Add new notification target for Send Money
    This adds a new send_money_landing target for notifications to
    appear on the landing page of the send money service.

--- mtp_api/apps/service/migrations/0007_notification_areas.py
+++ mtp_api/apps/service/migrations/0007_notification_areas.py
@@ -9,6 +9,20 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='notification',
             name='target',
-            field=models.CharField(choices=[('bankadmin_login', 'Bank admin: before login'), ('bankadmin_dashboard', 'Bank admin: dashboard'), ('cashbook_login', 'Cashbook: before login'), ('cashbook_dashboard', 'Cashbook: dashboard'), ('cashbook_all', 'Cashbook: all apps'), ('cashbook_cashbook', 'Cashbook: cashbook app'), ('cashbook_disbursements', 'Cashbook: disbursements app'), ('noms_ops_login', 'Noms Ops: before login'), ('noms_ops_security_dashboard', 'Noms Ops: security dashboard')], max_length=30),
+            field=models.CharField(
+                choices=[
+                    ('bankadmin_login', 'Bank admin: before login'),
+                    ('bankadmin_dashboard', 'Bank admin: dashboard'),
+                    ('cashbook_login', 'Cashbook: before login'),
+                    ('cashbook_dashboard', 'Cashbook: dashboard'),
+                    ('cashbook_all', 'Cashbook: all apps'),
+                    ('cashbook_cashbook', 'Cashbook: cashbook app'),
+                    ('cashbook_disbursements', 'Cashbook: disbursements app'),
+                    ('noms_ops_login', 'Noms Ops: before login'),
+                    ('noms_ops_security_dashboard', 'Noms Ops: security dashboard'),
+                    ('send_money_landing', 'Send Money: landing page'),
+                ],
+                max_length=30,
+            ),
         ),
     ]
