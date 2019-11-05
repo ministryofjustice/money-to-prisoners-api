b730965d5c86c4659e3b98e77cb727a7f5fd9ed7
Marco Fucci <info@marcofucci.com>
2019-11-05 13:32:06 +0000

    Remove nomis.get_client_token monkeypatch

--- mtp_api/apps/core/app.py
+++ mtp_api/apps/core/app.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig as DjangoAppConfig
-from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 
@@ -13,18 +12,3 @@ class AppConfig(DjangoAppConfig):
         from core.admin import site
         admin.site = site
         admin_sites.site = site
-
-        from mtp_common import nomis
-        nomis.get_client_token = get_client_token
-
-
-def get_client_token():
-    if getattr(settings, 'NOMIS_API_CLIENT_TOKEN', ''):
-        return settings.NOMIS_API_CLIENT_TOKEN
-
-    from core.models import Token
-
-    try:
-        return Token.objects.get(name='nomis').token
-    except Token.DoesNotExist:
-        pass
