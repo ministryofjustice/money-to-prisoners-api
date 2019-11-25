1272fa152730cb5621833773000dcbefcbcda767
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-11-25 12:31:28 +0000

    Add pod's cluster IP address to allowed hosts to allow prometheus to access metrics

--- mtp_api/settings/docker.py
+++ mtp_api/settings/docker.py
@@ -1,6 +1,8 @@
 """
 Docker settings
 """
+from mtp_common.stack import get_current_pod
+
 from mtp_api.settings.base import *  # noqa
 from mtp_api.settings.base import ENVIRONMENT, os
 
@@ -15,6 +17,10 @@ ALLOWED_HOSTS = [
     '.service.justice.gov.uk',
     '.svc.cluster.local',
 ]
+
+current_pod = get_current_pod()
+if current_pod and current_pod.status.pod_ip:
+    ALLOWED_HOSTS.append(current_pod.status.pod_ip)
 
 # security tightening
 if ENVIRONMENT != 'local':
