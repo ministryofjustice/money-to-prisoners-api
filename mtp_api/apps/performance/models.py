f4b793d0cb9dc4187695c699ae1f7d88fa893d9a
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-04-09 20:25:10 +0100

    Allow monthly digital take-up calculation to exclude private estate

--- mtp_api/apps/performance/models.py
+++ mtp_api/apps/performance/models.py
@@ -9,33 +9,61 @@ from core import dictfetchall, mean
 
 
 class DigitalTakeupManager(models.Manager):
-    def digital_takeup_per_month(self, since=None):
+    def digital_takeup_per_month(self, since=None, exclude_private_estate=False):
         """
         Per-month digital take-up averages
         """
         if since is None:
             today = datetime.date.today()
             since = max(DigitalTakeup.reports_start, today.replace(year=today.year - 2, month=1, day=1))
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                WITH credit_count AS (
-                  SELECT date_trunc('month', received_at) AS date,
+
+        if exclude_private_estate:
+            included_prisons_sql = """
+                included_prisons AS (
+                    SELECT prison_prison.nomis_id as nomis_id
+                    FROM prison_prison
+                    WHERE prison_prison.private_estate IS false
+                ),
+            """
+            credit_count_join_sql = """
+                JOIN included_prisons ON included_prisons.nomis_id = credit_credit.prison_id
+            """
+            average_takeup_join_sql = """
+                JOIN included_prisons ON included_prisons.nomis_id = performance_digitaltakeup.prison_id
+            """
+        else:
+            included_prisons_sql = ''
+            credit_count_join_sql = ''
+            average_takeup_join_sql = ''
+
+        sql = f"""
+        WITH
+            {included_prisons_sql}
+            credit_count AS (
+                SELECT date_trunc('month', received_at) AS date,
                     COUNT(*) AS accurate_credits_by_mtp
-                  FROM credit_credit
-                  WHERE resolution = 'credited' AND received_at >= %(since)s
-                  GROUP BY date_trunc('month', received_at)
-                ), average_takeup AS (
-                  SELECT date_trunc('month', date) AS date,
+                FROM credit_credit
+                {credit_count_join_sql}
+                WHERE resolution = 'credited' AND received_at >= %(since)s
+                GROUP BY date_trunc('month', received_at)
+            ),
+            average_takeup AS (
+                SELECT date_trunc('month', date) AS date,
                     SUM(credits_by_post) AS reported_credits_by_post,
                     SUM(credits_by_mtp) AS reported_credits_by_mtp
-                  FROM performance_digitaltakeup
-                  WHERE date >= %(since)s
-                  GROUP BY date_trunc('month', date)
-                )
-                SELECT credit_count.date, accurate_credits_by_mtp, reported_credits_by_post, reported_credits_by_mtp
-                FROM credit_count FULL OUTER JOIN average_takeup ON credit_count.date = average_takeup.date
-                ORDER BY credit_count.date
-            """, params={'since': since})
+                FROM performance_digitaltakeup
+                {average_takeup_join_sql}
+                WHERE date >= %(since)s
+                GROUP BY date_trunc('month', date)
+            )
+        SELECT credit_count.date, accurate_credits_by_mtp, reported_credits_by_post, reported_credits_by_mtp
+        FROM credit_count
+        FULL OUTER JOIN average_takeup ON credit_count.date = average_takeup.date
+        ORDER BY credit_count.date
+        """
+
+        with connection.cursor() as cursor:
+            cursor.execute(sql, params={'since': since})
             return dictfetchall(cursor)
 
 
