8a2865ea0537eda72ec30ce070cfd8ce06fc6fbb
mattbee <matt.bee@madetech.com>
2020-08-11 13:29:32 +0100

    MTP-1513 Set up parallelism in circleCI pipeline
    Co-author: Matt Bee <matt.bee@digital.justice.gov.uk>
    
    MTP-1513 Moved logic to coerce test directories to string to /bin/sh invokcation outside the docker run bash command
    
    MTP-1513 Copied parrelism definition to parent yaml object
    
    MTP-1513 Transform test filepaths into modulepaths instead of using directories
    
    MTP-1513 First attempted at changing steps to jobs (WIP)
    
    MTP-1513 Added dependencies between jobs and factored out config into yaml against executor
    
    MTP-1513 Refacted environment variable functionality out into orb
    
    MTP-1513 Added shorthand step names back in
    
    MTP-1513 Removed orb functionality in favour of circleci recommended method of env persistence
    
    https://circleci.com/docs/2.0/env-vars/#using-parameters-and-bash-environment
    
    MTP-1513 Removed bash variables from CircleCI pipeline
    
    MTP-1513 Reverted previous change to remove variables, and instead moved all dependant functionality out into shell files with the variables defined in a sourced common.sh
    
    MTP-1513 added setup_remote_docker to steps to see if this explicit reference to remote docker is what provisions the docker socket in the build env
    
    MTP-1593 Condensed unnecessarily granular image build stages into one
    
    MTP-1513 Attempt at sharing docker images cheaply between build jobs without having overhead of explicitly serializing to filesytem
    
    This is necessary because we need the parralelised test job to access the images without having to rebuild them for each test instance
    
    MTP-1513 working around lack of image sharing between jobs by explicitly serializing image to file and deserializing in relevant steps

---
+++ .circleci/custom_scripts/ecr_cleanup.sh
@@ -0,0 +1,24 @@
+#!/bin/env sh
+. ${CIRCLE_WORKING_DIRECTORY}/.circleci/custom_scripts/common.sh
+echo "Cleaning ECR"
+
+delete_images() {
+  if [[ "${IMAGES_TO_DELETE}" ]]; then
+    AWS_ECR_ARGS=
+    for IMAGE_TO_DELETE in ${IMAGES_TO_DELETE}; do
+      AWS_ECR_ARGS="${AWS_ECR_ARGS} imageDigest=${IMAGE_TO_DELETE}"
+    done
+    aws ecr batch-delete-image --repository-name prisoner-money/money-to-prisoners --image-ids ${AWS_ECR_ARGS}
+  fi
+}
+
+echo "Deleting untagged images"
+IMAGES_TO_DELETE=$(aws ecr list-images --repository-name prisoner-money/money-to-prisoners --filter tagStatus=UNTAGGED --query 'imageIds[*].imageDigest' --output text)
+delete_images
+
+if [[ "${CIRCLE_BRANCH}" != "master" ]]; then
+  echo "Deleting other images from branch ${CIRCLE_BRANCH}"
+  IMAGES_TO_DELETE=$(aws ecr describe-images --repository-name prisoner-money/money-to-prisoners --query 'imageDetails[?contains(map(&starts_with(@, '"'"${app}.${CIRCLE_BRANCH_LOWERCASE}."'"'), @.imageTags), `true`) && ! contains(@.imageTags, '"'"${tag}"'"')].imageDigest' --output text)
+  delete_images
+fi
+
---
+++ .circleci/custom_scripts/pull_base_image.sh
@@ -0,0 +1,5 @@
+#!/bin/env sh
+. ${CIRCLE_WORKING_DIRECTORY}/.circleci/custom_scripts/common.sh
+aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${ECR_ENDPOINT}
+docker pull ${registry}:base-web
+docker tag ${registry}:base-web base-web
