c52220c0d272eff14399cce508cae1d5e51c7ec8
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-02-09 14:05:34 +0000

    Fix migration linked to change in aabb048cb409a9622496c15baee1862212ce5173 This has no effect on the database fields in postgres so a fix-up to an old migration is valid; it's not necessary to make a new migration and deploy.

--- mtp_api/apps/security/migrations/0031_check_rejection_reasons.py
+++ mtp_api/apps/security/migrations/0031_check_rejection_reasons.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='check',
             name='rejection_reasons',
-            field=django.contrib.postgres.fields.jsonb.JSONField(default={}),
+            field=django.contrib.postgres.fields.jsonb.JSONField(default=dict),
         ),
     ]
