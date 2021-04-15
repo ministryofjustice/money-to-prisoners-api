d60b36eb2b79a012defeb46b6691ef47c75451aa
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-04-15 18:09:37 +0100

    Improve data-dumping and uploading command for Analytical Platform by: - making tests check that expected parameters are used in called commands - removing round-trip of file paths from `str` → `Path` → `str` - not calculating today's date 5 times

--- mtp_api/apps/core/management/commands/dump_and_upload_for_ap.py
+++ mtp_api/apps/core/management/commands/dump_and_upload_for_ap.py
@@ -1,9 +1,10 @@
 import datetime
-import pathlib
+import os
 import tempfile
 import textwrap
 
 from django.core.management import BaseCommand, call_command
+from django.utils import timezone
 
 
 class Command(BaseCommand):
@@ -14,34 +15,18 @@ class Command(BaseCommand):
     help = textwrap.dedent(__doc__).strip()
 
     def handle(self, *args, **options):
+        today = timezone.localtime().date()
+        yesterday = today - datetime.timedelta(days=1)
+
+        today = today.strftime('%Y-%m-%d')
+        yesterday = yesterday.strftime('%Y-%m-%d')
+        date_range = {
+            'after': yesterday,
+            'before': today,
+        }
+
         with tempfile.TemporaryDirectory() as temp_path:
-
-            file_path_credits = pathlib.Path(temp_path) / 'credits'
-            file_path_disbursements = pathlib.Path(temp_path) / 'disbursements'
-
-            date = todays_date()
-
-            dump_for_ap_command(str(file_path_credits), str(file_path_disbursements))
-            upload_dump_for_ap_command(str(file_path_credits), str(file_path_disbursements), date)
-
-
-def dump_for_ap_command(file_path_credits, file_path_disbursements):
-    yesterday = yesterdays_date()
-    today = todays_date()
-
-    call_command('dump_for_ap', 'credits', file_path_credits, after=yesterday, before=today)
-    call_command('dump_for_ap', 'disbursements', file_path_disbursements, after=yesterday, before=today)
-
-
-def upload_dump_for_ap_command(file_path_credits, file_path_disbursements, date):
-    call_command('upload_dump_for_ap', file_path_credits, (f'{date}_credits'))
-    call_command('upload_dump_for_ap', file_path_disbursements, (f'{date}_disbursements'))
-
-
-def todays_date():
-    return datetime.date.today().strftime('%Y-%m-%d')
-
-
-def yesterdays_date():
-    date = datetime.date.today() - datetime.timedelta(days=1)
-    return date.strftime('%Y-%m-%d')
+            for record_type in ['credits', 'disbursements']:
+                file_path = os.path.join(temp_path, record_type)
+                call_command('dump_for_ap', record_type, file_path, **date_range)
+                call_command('upload_dump_for_ap', file_path, f'{today}_{record_type}')
--- mtp_api/apps/core/tests/test_dump_and_upload_for_ap.py
+++ mtp_api/apps/core/tests/test_dump_and_upload_for_ap.py
@@ -1,24 +1,52 @@
+import datetime
 from unittest import mock
 
 from django.core.management import call_command
 from django.test import TestCase, override_settings
+from django.utils import timezone
+
+from core.management.commands.dump_for_ap import Command as DumpCommand, Serialiser
+from core.management.commands.upload_dump_for_ap import Command as UploadCommand
 
 
+@override_settings(ANALYTICAL_PLATFORM_BUCKET='my-bucket')
 class TestDumpAndUploadForAp(TestCase):
-    @mock.patch('core.management.commands.dump_and_upload_for_ap.upload_dump_for_ap_command')
-    @mock.patch('core.management.commands.dump_and_upload_for_ap.dump_for_ap_command')
-    def test_dump_for_ap_command_is_called(self,
-                                           mock_dump_for_ap_command,
-                                           mock_upload_dump_for_ap_command):
+    @mock.patch.object(UploadCommand, 'handle')
+    @mock.patch.object(DumpCommand, 'handle')
+    @mock.patch('core.management.commands.dump_and_upload_for_ap.timezone')
+    def test_dump_for_ap_command_calls_appropriate_commands(
+        self,
+        mocked_timezone,
+        mocked_dump_for_ap,
+        mocked_upload_dump_for_ap,
+    ):
+        mocked_timezone.localtime.return_value = datetime.datetime(
+            2021, 3, 1, 13, 15,
+            tzinfo=timezone.get_default_timezone()
+        )
+        mocked_dump_for_ap.return_value = None
+        mocked_upload_dump_for_ap.return_value = None
 
         call_command('dump_and_upload_for_ap')
 
-        assert mock_dump_for_ap_command.called
+        expected_record_types = set(Serialiser.serialisers)
 
-    @mock.patch('core.management.commands.dump_and_upload_for_ap.upload_dump_for_ap_command')
-    @override_settings(ANALYTICAL_PLATFORM_BUCKET='my-bucket')
-    def test_upload_dump_for_ap_command_is_called(self, mock_upload_for_ap_command):
+        record_types = set()
+        for call in mocked_dump_for_ap.call_args_list:
+            handle_kwargs = call[1]
+            # ensure correct date range used
+            self.assertEqual(handle_kwargs['after'], '2021-02-28')
+            self.assertEqual(handle_kwargs['before'], '2021-03-01')
+            record_types.add(handle_kwargs['type'])
+        # ensure all record types are exported
+        self.assertSetEqual(record_types, expected_record_types)
 
-        call_command('dump_and_upload_for_ap')
-
-        assert mock_upload_for_ap_command.called
+        record_types = set()
+        for call in mocked_upload_dump_for_ap.call_args_list:
+            handle_kwargs = call[1]
+            object_name = handle_kwargs['object_name']
+            # ensure S3 objects are named correctly
+            self.assertTrue(object_name.startswith('2021-03-01_'))
+            record_types.add(object_name[11:])
+        # ensure all S3 objects are named correctly and all record types are covered
+        self.assertSetEqual(record_types, expected_record_types)
