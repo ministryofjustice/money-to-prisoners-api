ef2dc1fbc2031b5c60dbc34ace9bc9c2baa6248c
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2021-06-22 15:01:21 +0100

    Added PerformanceData admin page

--- mtp_api/apps/performance/admin.py
+++ mtp_api/apps/performance/admin.py
@@ -3,8 +3,8 @@ from django.contrib.admin import ModelAdmin
 from django.utils.translation import gettext_lazy as _
 
 from core.admin import add_short_description
-from performance.models import DigitalTakeup, UserSatisfaction
-
+from performance.models import DigitalTakeup, PerformanceData, UserSatisfaction
+from transaction.utils import format_percentage
 
 @admin.register(DigitalTakeup)
 class DigitalTakeupAdmin(ModelAdmin):
@@ -23,3 +23,31 @@ class UserSatisfactionAdmin(ModelAdmin):
     list_display = ('date', *UserSatisfaction.rating_field_names)
     ordering = ('-date',)
     date_hierarchy = 'date'
+
+
+@admin.register(PerformanceData)
+class PerformanceDataAdmin(ModelAdmin):
+    list_display = (
+        'week',
+        'credits_total',
+        'credits_by_mtp',
+        'view_digital_takeup',
+        'view_completion_rate',
+        'view_user_satisfaction',
+        *PerformanceData.rating_field_names,
+    )
+    ordering = ('-week',)
+    date_hierarchy = 'week'
+
+    def view_digital_takeup(self, obj):
+        return self._as_percentage(obj.digital_takeup)
+
+    def view_completion_rate(self, obj):
+        return self._as_percentage(obj.completion_rate)
+
+    def view_user_satisfaction(self, obj):
+        return self._as_percentage(obj.user_satisfaction)
+
+    def _as_percentage(self, value):
+        if value:
+            return format_percentage(value)
