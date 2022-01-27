a71a0ebf88a2d2ecdc8b6a276074268ede0f4033
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-01-27 11:54:19 +0000

    Update app build, deploy and cleanup pipelines to use `main` as the default branch name

--- .circleci/custom_scripts/ecr_cleanup.sh
+++ .circleci/custom_scripts/ecr_cleanup.sh
@@ -16,7 +16,7 @@ echo "Deleting untagged images"
 IMAGES_TO_DELETE=$(aws ecr list-images --repository-name prisoner-money/money-to-prisoners --filter tagStatus=UNTAGGED --query 'imageIds[*].imageDigest' --output text)
 delete_images
 
-if [[ "${CIRCLE_BRANCH}" != "master" ]]; then
+if [[ "${CIRCLE_BRANCH}" != "main" ]]; then
   echo "Deleting other images from branch ${CIRCLE_BRANCH}"
   IMAGES_TO_DELETE=$(aws ecr describe-images --repository-name prisoner-money/money-to-prisoners --query 'imageDetails[?contains(map(&starts_with(@, '"'"${app}.${CIRCLE_BRANCH_LOWERCASE}."'"'), @.imageTags), `true`) && ! contains(@.imageTags, '"'"${tag}"'"')].imageDigest' --output text)
   delete_images
