01f132e1adc0422b4a6f76d5907dc570c38363bf
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2022-07-26 18:21:35 +0100

    MTP-1921: remove setting cloud_RoleInstance
    It's defaulted here: https://github.com/census-instrumentation/opencensus-python/blob/de44767bd3cfc8181709af896772483001ecb812/contrib/opencensus-ext-azure/opencensus/ext/azure/common/utils.py#L28 which should be the k8 pod name

--- mtp_api/apps/core/app_insights_logger.py
+++ mtp_api/apps/core/app_insights_logger.py
@@ -11,5 +11,4 @@ def callback_add_role_name(envelope):
     """ Callback function for opencensus """
     """ This configures cloud_RoleName """
     envelope.tags['ai.cloud.role'] = 'mtp-api'
-    envelope.tags['ai.cloud.roleInstance'] = 'mtp-api'
     return True
