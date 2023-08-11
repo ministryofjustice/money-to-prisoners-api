c667f76ca4915d24ee116407d304bc78eae6f265
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-08-11 15:39:53 +0100

    Update CircleCI and Github Actions workflows to work without ECR IAM access key

--- .circleci/custom_scripts/common.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-export CIRCLE_BRANCH_LOWERCASE=$(echo $CIRCLE_BRANCH | tr '[:upper:]' '[:lower:]')
-
-export registry=${ECR_ENDPOINT}/prisoner-money/money-to-prisoners
-export version=${CIRCLE_BRANCH_LOWERCASE}.${CIRCLE_SHA1:0:7}
-export tag=${app}.${version}
