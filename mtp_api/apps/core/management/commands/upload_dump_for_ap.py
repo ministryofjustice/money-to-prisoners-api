30a0e81d9bd77639ee2bdee53fb4ffce5df3d157
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-08-14 17:44:07 +0100

    Remove unused IAM-role-assuming step when uploading data to Analytical Platform S3 bucket

--- mtp_api/apps/core/management/commands/upload_dump_for_ap.py
+++ mtp_api/apps/core/management/commands/upload_dump_for_ap.py
@@ -1,4 +1,3 @@
-import os
 import textwrap
 
 import boto3
@@ -25,27 +24,10 @@ class Command(BaseCommand):
         upload_file_to_analytical_platform(options['file_path'], options['object_name'])
 
 
-def get_aws_credentials():
-    if settings.AWS_IAM_ROLE_ARN:
-        sts_client = boto3.client('sts')
-        session_name = os.environ.get('POD_NAME') or settings.APP
-        session_name = f'mtp-{session_name}'
-        assumed_role = sts_client.assume_role(
-            RoleArn=settings.AWS_IAM_ROLE_ARN, RoleSessionName=session_name,
-        )
-        credentials = assumed_role['Credentials']
-        return {
-            'aws_access_key_id': credentials['AccessKeyId'],
-            'aws_secret_access_key': credentials['SecretAccessKey'],
-            'aws_session_token': credentials['SessionToken'],
-        }
-    return {}
-
-
 def upload_file_to_analytical_platform(file_path, target_name):
     if settings.ANALYTICAL_PLATFORM_BUCKET_PATH:
         target_name = f'{settings.ANALYTICAL_PLATFORM_BUCKET_PATH}/{target_name}'
-    s3_client = boto3.client('s3', **get_aws_credentials())
+    s3_client = boto3.client('s3')
     s3_client.upload_file(
         Filename=file_path, Bucket=settings.ANALYTICAL_PLATFORM_BUCKET, Key=target_name,
         ExtraArgs={'ACL': 'bucket-owner-full-control', 'ServerSideEncryption': 'AES256'},
