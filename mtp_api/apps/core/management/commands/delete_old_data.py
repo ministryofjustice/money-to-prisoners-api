dfc5c1d2749780b6914caed0baf178b1f62ac7fe
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2023-12-05 11:22:16 +0000

    MTP-1970: Show deletion/updates tree when logging
    Rather than show all log lines at the same level in a flat manner,
    The `delete_old_data` command now shows these logs in a nested way.
    
    This is useful to see the relationship between the deleted/updated
    records and what triggerred them.
    
    For example:
    
    ```
    Deleting Credits older than 2016-01-03...
    	[...]
    	Deleting Credit Credit 4, £40 Mrs. Walls > JOHN WALLS, credited...
    		Deleting associated Events...
    			Deleting Event Event object (5)...
    				Records deleted: (2, {'notification.CreditEvent': 1, 'notification.Event': 1}).
    		Records deleted: (3, {'payment.Payment': 1, 'credit.Log': 1, 'credit.Credit': 1}).
    		SenderProfile Sender 2 has no credits, deleting...
    			Deleting associated Events...
    				Deleting Event Event object (10)...
    					Records deleted: (2, {'notification.SenderProfileEvent': 1, 'notification.Event': 1}).
    			Deleting SenderProfile's SavedSearches '*/senders/2/*'...
    				Records deleted: (1, {'security.SavedSearch': 1}).
    			Records deleted: (3, {'security.SenderEmail': 1, 'security.DebitCardSenderDetails': 1, 'security.SenderProfile': 1}).
    		PrisonerProfile Prisoner 2 (B1510BF) updated.
        [...]
    ```
    
    By looking at this output it's clear - even without knowing much about
    MTP data model - that Credit 4 was deleted and that triggered the deletion
    of the related events/payments.
    The SenderProfile associate with that credit was also deleted because
    it has no other credits. This in turn caused the deletion of events,
    saved searches and other related records. But also cause triggered the
    update of the PrisonerProfile 2 (its counts for credits could have changed)
    
    Hopefully this will make it easier to follow the chain of deletions both
    when we'll test this against production data but also when this will run
    in production.

--- mtp_api/apps/core/management/commands/delete_old_data.py
+++ mtp_api/apps/core/management/commands/delete_old_data.py
@@ -24,10 +24,13 @@ class Command(BaseCommand):
     """
     help = textwrap.dedent(__doc__).strip()
 
+    def print_message(self, message, depth = 0):
+        self.write('\t' * depth + message)
+
     def handle(self, *args, **options):
         verbosity = options['verbosity']
 
-        self.print_message = self.stdout.write if verbosity else lambda m: m
+        self.write = self.stdout.write if verbosity else lambda m: m
 
         today = timezone.localdate()
         seven_years_ago = today - datetime.timedelta(days=7*365)
@@ -44,63 +47,65 @@ class Command(BaseCommand):
 
         self.print_message(f'\nDeleting Transactions older than {seven_years_ago}...')
         records_deleted = Transaction.objects.filter(received_at__lt=seven_years_ago).delete()
-        self.print_message(f'Records deleted: {records_deleted}.')
+        self.print_message(f'Records deleted: {records_deleted}.', 1)
 
         self.print_message(f'\nDeleting Payment older than {seven_years_ago}...')
         records_deleted = Payment.objects.filter(modified__lt=seven_years_ago).delete()
-        self.print_message(f'Records deleted: {records_deleted}.')
+        self.print_message(f'Records deleted: {records_deleted}.', 1)
 
     @transaction.atomic
     def delete_credit(self, credit: Credit):
-        self.print_message(f'Deleting Credit {credit}...')
+        depth = 1
+        self.print_message(f'Deleting Credit {credit}...', depth)
 
-        self.print_message(f'Deleting associated Events...')
-        self.delete_events(credit.creditevent_set.all())
+        self.print_message(f'Deleting associated Events...', depth+1)
+        self.delete_events(credit.creditevent_set.all(), depth+2)
 
         record_deleted = credit.delete()
-        self.print_message(f'Records deleted: {record_deleted}.')
+        self.print_message(f'Records deleted: {record_deleted}.', depth+1)
 
         sender_profile = credit.sender_profile
         if sender_profile:
             if not sender_profile.credits.exists():
-                self.print_message(f'SenderProfile {sender_profile} has no credits, deleting...')
+                self.print_message(f'SenderProfile {sender_profile} has no credits, deleting...', depth+1)
 
-                self.print_message(f'Deleting associated Events...')
-                self.delete_events(sender_profile.senderprofileevent_set.all())
+                self.print_message(f'Deleting associated Events...', depth+2)
+                self.delete_events(sender_profile.senderprofileevent_set.all(), depth+3)
 
-                self.print_message(f"Deleting SenderProfile's SavedSearches '*/senders/{sender_profile.id}/*'...")
+                self.print_message(f"Deleting SenderProfile's SavedSearches '*/senders/{sender_profile.id}/*'...", depth+2)
                 record_deleted = SavedSearch.objects \
                     .filter(site_url__contains=f'/senders/{sender_profile.id}/') \
                     .delete()
-                self.print_message(f'Records deleted: {record_deleted}.')
+                self.print_message(f'Records deleted: {record_deleted}.', depth+3)
 
                 record_deleted = sender_profile.delete()
-                self.print_message(f'Records deleted: {record_deleted}.')
+                self.print_message(f'Records deleted: {record_deleted}.', depth+2)
             else:
                 SenderProfile.objects \
                     .filter(id=sender_profile.id) \
                     .recalculate_totals()
-                self.print_message(f'SenderProfile {sender_profile} updated.')
+                self.print_message(f'SenderProfile {sender_profile} updated.', depth+1)
 
-        self.update_or_delete_prisoner_profile(credit.prisoner_profile)
+        self.update_or_delete_prisoner_profile(credit.prisoner_profile, depth+1)
 
     @transaction.atomic
     def delete_disbursement(self, disbursement: Disbursement):
-        self.print_message(f'Deleting Disbursement {disbursement}...')
+        depth = 1
+        self.print_message(f'Deleting Disbursement {disbursement}...', depth)
 
-        self.print_message(f'Deleting associated Events...')
-        self.delete_events(disbursement.disbursementevent_set.all())
+        self.print_message(f'Deleting associated Events...', depth+1)
+        self.delete_events(disbursement.disbursementevent_set.all(), depth+2)
 
         record_deleted = disbursement.delete()
-        self.print_message(f'Records deleted: {record_deleted}.')
+        self.print_message(f'Records deleted: {record_deleted}.', depth+1)
 
         recipient_profile = disbursement.recipient_profile
         if recipient_profile:
             if not recipient_profile.disbursements.exists():
-                self.print_message(f'RecipientProfile {recipient_profile} has no disbursements, deleting...')
+                self.print_message(f'RecipientProfile {recipient_profile} has no disbursements, deleting...', depth+1)
 
-                self.print_message(f'Deleting associated Events...')
-                self.delete_events(recipient_profile.recipientprofileevent_set.all())
+                self.print_message(f'Deleting associated Events...', depth+2)
+                self.delete_events(recipient_profile.recipientprofileevent_set.all(), depth+3)
 
                 for recipient_details in recipient_profile.bank_transfer_details.all():
                     bank_account = recipient_details.recipient_bank_account
@@ -108,45 +113,45 @@ class Command(BaseCommand):
                     #       BankTransferRecipientDetails will be cascade-deleted anyway when the RecipientProfile
                     #       will be deleted, so no point in checking if bank_account.recipients is empty.
                     if not bank_account.senders.exists():
-                        self.print_message(f'BankAccount {bank_account} has no senders, deleting...')
+                        self.print_message(f'BankAccount {bank_account} has no senders, deleting...', depth+2)
                         record_deleted = bank_account.delete()
-                        self.print_message(f'Records deleted: {record_deleted}.')
+                        self.print_message(f'Records deleted: {record_deleted}.', depth+3)
 
                 record_deleted = recipient_profile.delete()
-                self.print_message(f'Records deleted: {record_deleted}.')
+                self.print_message(f'Records deleted: {record_deleted}.', depth+2)
             else:
                 RecipientProfile.objects \
                     .filter(id=recipient_profile.id) \
                     .recalculate_totals()
-                self.print_message(f'RecipientProfile {recipient_profile} updated.')
+                self.print_message(f'RecipientProfile {recipient_profile} updated.', depth+1)
 
-        self.update_or_delete_prisoner_profile(disbursement.prisoner_profile)
+        self.update_or_delete_prisoner_profile(disbursement.prisoner_profile, depth+1)
 
-    def update_or_delete_prisoner_profile(self, prisoner_profile: PrisonerProfile | None):
+    def update_or_delete_prisoner_profile(self, prisoner_profile: PrisonerProfile | None, depth):
         if prisoner_profile:
             if not prisoner_profile.credits.exists() and not prisoner_profile.disbursements.exists():
-                self.print_message(f'PrisonerProfile {prisoner_profile} has no credits nor disbursements, deleting...')
+                self.print_message(f'PrisonerProfile {prisoner_profile} has no credits nor disbursements, deleting...', depth)
 
-                self.print_message(f'Deleting associated Events...')
-                self.delete_events(prisoner_profile.prisonerprofileevent_set.all())
+                self.print_message(f'Deleting associated Events...', depth+1)
+                self.delete_events(prisoner_profile.prisonerprofileevent_set.all(), depth+2)
 
-                self.print_message(f"Deleting PrisoneProfile's SavedSearches '*/prisoners/{prisoner_profile.id}/*'...")
+                self.print_message(f"Deleting PrisoneProfile's SavedSearches '*/prisoners/{prisoner_profile.id}/*'...", depth+1)
                 record_deleted = SavedSearch.objects \
                     .filter(site_url__contains=f'/prisoners/{prisoner_profile.id}/') \
                     .delete()
-                self.print_message(f'Records deleted: {record_deleted}.')
+                self.print_message(f'Records deleted: {record_deleted}.', depth+2)
 
                 record_deleted = prisoner_profile.delete()
-                self.print_message(f'Records deleted: {record_deleted}.')
+                self.print_message(f'Records deleted: {record_deleted}.', depth+1)
             else:
                 PrisonerProfile.objects \
                             .filter(id=prisoner_profile.id) \
                             .recalculate_totals()
-                self.print_message(f'PrisonerProfile {prisoner_profile} updated.')
+                self.print_message(f'PrisonerProfile {prisoner_profile} updated.', depth)
 
-    def delete_events(self, thing_events: Sequence[CreditEvent | DisbursementEvent | PrisonerProfileEvent | RecipientProfileEvent |SenderProfileEvent]):
+    def delete_events(self, thing_events: Sequence[CreditEvent | DisbursementEvent | PrisonerProfileEvent | RecipientProfileEvent |SenderProfileEvent], depth):
         for thing_event in thing_events:
             event = thing_event.event
-            self.print_message(f'Deleting Event {event}...')
+            self.print_message(f'Deleting Event {event}...', depth)
             record_deleted = event.delete()
-            self.print_message(f'Records deleted: {record_deleted}.')
+            self.print_message(f'Records deleted: {record_deleted}.', depth+1)
