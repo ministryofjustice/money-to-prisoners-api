4ceefd3885acbd848345f9f874374377dd472d8e
Igor <igor.ushkarev1@justice.gov.uk>
2025-10-28 16:23:51 +0000

    Update dependencies

--- mtp_api/apps/credit/__init__.py
+++ mtp_api/apps/credit/__init__.py
@@ -1,4 +1,4 @@
 class InvalidCreditStateException(Exception):
-    def __init__(self, conflict_ids, *args, **kwargs):
+    def __init__(self, conflict_ids, *args, **kwargs):  # noqa: B042
         self.conflict_ids = conflict_ids
         super().__init__(*args, **kwargs)
